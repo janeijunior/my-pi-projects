5f34cc039740eea9f29cac08f44a1a7dbc22aaa4
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-08-17 22:19:18 -0300

    Modified Adafruit_MCP230xx.py

--- Adafruit_MCP230xx/Adafruit_MCP230xx.py
+++ Adafruit_MCP230xx/Adafruit_MCP230xx.py
@@ -246,13 +246,22 @@ if __name__ == '__main__':
       mcp.output(9, 1) # Pin 0 High 
       time.sleep(1.2) 
       mcp.output(1, 0) # Pin 1 Low 
+      time.sleep(1.2)
       mcp.output(0, 0) # Pin 1 Low 
+      time.sleep(1.2)
       mcp.output(2, 0) # Pin 0 High 
+      time.sleep(1.2)
       mcp.output(3, 0) # Pin 0 High 
+      time.sleep(1.2)
       mcp.output(4, 0) # Pin 0 High 
+      time.sleep(1.2)
       mcp.output(5, 0) # Pin 0 High 
+      time.sleep(1.2)
       mcp.output(6, 0) # Pin 0 High 
-      mcp.output(7, 0) # Pin 0 High 
+      time.sleep(1.2)
+      mcp.output(7, 0) # Pin 0 High
+      time.sleep(1.2)
       mcp.output(8, 0) # Pin 0 High 
+      time.sleep(1.2)
       mcp.output(9, 0) # Pin 0 High 
       time.sleep(1.2)
