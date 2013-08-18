7e5e5d6d99ae32e7d1907461aa5fb90ea91b6a80
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-08-18 09:04:35 -0300

    Modified Adafruit_MCP230xx.py

--- Adafruit_MCP230xx/Adafruit_MCP230xx.py
+++ Adafruit_MCP230xx/Adafruit_MCP230xx.py
@@ -227,44 +227,48 @@ if __name__ == '__main__':
     
     # Python speed test on output 0 toggling at max speed
     #while True:
-    time.sleep(1)
-    mcp.output(0, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(1, 1) # Pin 0 High
-    time.sleep(0.5)
-    mcp.output(2, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(3, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(4, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(5, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(6, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(7, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(8, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(9, 1) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(9, 0) # Pin 1 Low 
-    time.sleep(0.5)
-    mcp.output(8, 0) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(7, 0) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(6, 0) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(5, 0) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(4, 0) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(3, 0) # Pin 0 High
-    time.sleep(0.5)
-    mcp.output(2, 0) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(1, 0) # Pin 0 High 
-    time.sleep(0.5)
-    mcp.output(0, 0) # Pin 1 Low 
-    time.sleep(0.5)
+    mcp.output(9, 1)
+    time.sleep(5)
+    mcp.output(9, 0)
+    
+#    time.sleep(1)
+#    mcp.output(0, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(1, 1) # Pin 0 High
+#    time.sleep(0.5)
+#    mcp.output(2, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(3, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(4, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(5, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(6, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(7, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(8, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(9, 1) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(9, 0) # Pin 1 Low 
+#    time.sleep(0.5)
+#    mcp.output(8, 0) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(7, 0) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(6, 0) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(5, 0) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(4, 0) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(3, 0) # Pin 0 High
+#    time.sleep(0.5)
+#    mcp.output(2, 0) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(1, 0) # Pin 0 High 
+#    time.sleep(0.5)
+#    mcp.output(0, 0) # Pin 1 Low 
+#    time.sleep(0.5)
