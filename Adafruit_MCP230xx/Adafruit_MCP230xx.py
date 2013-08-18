54f3fc030f83315ebcff0aef9dba21f0b0512ba3
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-08-18 09:05:28 -0300

    Modified Adafruit_MCP230xx.py

--- Adafruit_MCP230xx/Adafruit_MCP230xx.py
+++ Adafruit_MCP230xx/Adafruit_MCP230xx.py
@@ -228,6 +228,7 @@ if __name__ == '__main__':
     # Python speed test on output 0 toggling at max speed
     #while True:
     mcp.output(9, 1)
+    mcp.output(9, )
     
 #    time.sleep(1)
 #    mcp.output(0, 1) # Pin 0 High 
