1923d9203104d8bc93482ee2f296e1b8cf91aaf6
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-08-25 11:55:02 -0300

    Modified servidor.py

--- Adafruit_MCP230xx/servidor.py
+++ Adafruit_MCP230xx/servidor.py
@@ -39,7 +39,7 @@ try:
  
         comando = msg.strip() 
  
-        if comando[2:3] == "l0":
+        if comando[2] == "l" and comando[2] == "1":
             mcp.output(0, 1)
         elif comando[2:3] == "l1":
             mcp.output(1, 1)
