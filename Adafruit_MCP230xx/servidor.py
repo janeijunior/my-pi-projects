156e1b740c30114bd24fcab9d032fc7d84976191
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-08-25 13:40:11 -0300

    Modified servidor.py

--- Adafruit_MCP230xx/servidor.py
+++ Adafruit_MCP230xx/servidor.py
@@ -85,29 +85,3 @@ try:
 except KeyboardInterrupt:   #   Trata o CTRL+C
     print "Saindo..."
     exit(0)
-
-
-#mcp.output(0, 1) # Pin High 
-#mcp.output(1, 1) 
-#mcp.output(2, 1)  
-#mcp.output(3, 1)  
-#mcp.output(4, 1)
-#mcp.output(5, 1) 
-#mcp.output(6, 1)  
-#mcp.output(7, 1)  
-#mcp.output(8, 1)
-#mcp.output(9, 1) 
-#time.sleep(3)
-#mcp.output(0, 0) # Pin Low 
-#mcp.output(1, 0) 
-#mcp.output(2, 0)
-#mcp.output(3, 0)
-#mcp.output(4, 0) 
-#mcp.output(5, 0) 
-#mcp.output(6, 0)
-#mcp.output(7, 0)
-#mcp.output(8, 0) 
-#mcp.output(9, 0)
-
-#print "mensagem recebida: "+ recebe
-#serv_socket.close()
