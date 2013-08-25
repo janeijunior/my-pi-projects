69c61cb93e8bca14f1ca88a1af9427f84c3ecc91
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-08-25 09:41:33 -0300

    Modified Servidor.py

--- Adafruit_MCP230xx/Servidor.py
+++ Adafruit_MCP230xx/Servidor.py
@@ -17,61 +17,7 @@ print 'conectado'
 print "aguardando mensagem"
 recebe = con.recv(1024)
 
-mcp = Adafruit_MCP230xx(address=0x20, num_gpios=16)
-
-# Set pins 0, 1, 2 as outputs
-mcp.config(0, mcp.OUTPUT)
-mcp.config(1, mcp.OUTPUT)
-mcp.config(2, mcp.OUTPUT)
-mcp.config(3, mcp.OUTPUT)
-mcp.config(4, mcp.OUTPUT)
-mcp.config(5, mcp.OUTPUT)
-mcp.config(6, mcp.OUTPUT)
-mcp.config(7, mcp.OUTPUT)
-mcp.config(8, mcp.OUTPUT)
-mcp.config(9, mcp.OUTPUT)
-
-time.sleep(1)
-mcp.output(0, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(1, 1) # Pin 0 High
-time.sleep(0.5)
-mcp.output(2, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(3, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(4, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(5, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(6, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(7, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(8, 1) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(9, 1) # Pin 0 High 
-time.sleep(3)
-mcp.output(0, 0) # Pin 1 Low 
-time.sleep(0.5)
-mcp.output(1, 0) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(2, 0) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(3, 0) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(4, 0) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(5, 0) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(6, 0) # Pin 0 High
-time.sleep(0.5)
-mcp.output(7, 0) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(8, 0) # Pin 0 High 
-time.sleep(0.5)
-mcp.output(9, 0) # Pin 1 Low 
-time.sleep(0.5)
+sudo python Adafruit_MCP230xx.py   
  
 print "mensagem recebida: "+ recebe
 serv_socket.close()
