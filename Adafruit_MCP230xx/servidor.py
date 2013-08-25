663da6bbaa4a7879388f458922f0ec508f8ed31a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-08-25 16:18:01 -0300

    Modified servidor.py

--- Adafruit_MCP230xx/servidor.py
+++ Adafruit_MCP230xx/servidor.py
@@ -39,49 +39,50 @@ try:
  
         comando = msg.strip() 
  
-        if comando[2] == "l" and comando[3] == "0":
-            mcp.output(0, 1)
-        elif comando[2] == "l" and comando[3] == "1":
-            mcp.output(1, 1)
-        elif comando[2] == "l" and comando[3] == "2":
-            mcp.output(2, 1)
-        elif comando[2] == "l" and comando[3] == "3":
-            mcp.output(3, 1)
-        elif comando[2] == "l" and comando[3] == "4":
-            mcp.output(4, 1)
-        elif comando[2] == "l" and comando[3] == "5":
-            mcp.output(5, 1)
-        elif comando[2] == "l" and comando[3] == "6":
-            mcp.output(6, 1)
-        elif comando[2] == "l" and comando[3] == "7":
-            mcp.output(7, 1)
-        elif comando[2] == "l" and comando[3] == "8":
-            mcp.output(8, 1)
-        elif comando[2] == "l" and comando[3] == "9":
-            mcp.output(9, 1)
-        elif comando[2] == "d" and comando[3] == "0":
-            mcp.output(0, 0)
-        elif comando[2] == "d" and comando[3] == "1":
-            mcp.output(1, 0)
-        elif comando[2] == "d" and comando[3] == "2":
-            mcp.output(2, 0)
-        elif comando[2] == "d" and comando[3] == "3":
-            mcp.output(3, 0)
-        elif comando[2] == "d" and comando[3] == "4":
-            mcp.output(4, 0)
-        elif comando[2] == "d" and comando[3] == "5":
-            mcp.output(5, 0)
-        elif comando[2] == "d" and comando[3] == "6":
-            mcp.output(6, 0)
-        elif comando[2] == "d" and comando[3] == "7":
-            mcp.output(7, 0)
-        elif comando[2] == "d" and comando[3] == "8":
-            mcp.output(8, 0)
-        elif comando[2] == "d" and comando[3] == "9":
-            mcp.output(9, 0)
-        else:
-            print "Comando invalido!"
- 
+        if len(comando) > 0
+            if comando[2] == "l" and comando[3] == "0":
+                mcp.output(0, 1)
+            elif comando[2] == "l" and comando[3] == "1":
+                mcp.output(1, 1)
+            elif comando[2] == "l" and comando[3] == "2":
+                mcp.output(2, 1)
+            elif comando[2] == "l" and comando[3] == "3":
+                mcp.output(3, 1)
+            elif comando[2] == "l" and comando[3] == "4":
+                mcp.output(4, 1)
+            elif comando[2] == "l" and comando[3] == "5":
+                mcp.output(5, 1)
+            elif comando[2] == "l" and comando[3] == "6":
+                mcp.output(6, 1)
+            elif comando[2] == "l" and comando[3] == "7":
+                mcp.output(7, 1)
+            elif comando[2] == "l" and comando[3] == "8":
+                mcp.output(8, 1)
+            elif comando[2] == "l" and comando[3] == "9":
+                mcp.output(9, 1)
+            elif comando[2] == "d" and comando[3] == "0":
+                mcp.output(0, 0)
+            elif comando[2] == "d" and comando[3] == "1":
+                mcp.output(1, 0)
+            elif comando[2] == "d" and comando[3] == "2":
+                mcp.output(2, 0)
+            elif comando[2] == "d" and comando[3] == "3":
+                mcp.output(3, 0)
+            elif comando[2] == "d" and comando[3] == "4":
+                mcp.output(4, 0)
+            elif comando[2] == "d" and comando[3] == "5":
+                mcp.output(5, 0)
+            elif comando[2] == "d" and comando[3] == "6":
+                mcp.output(6, 0)
+            elif comando[2] == "d" and comando[3] == "7":
+                mcp.output(7, 0)
+            elif comando[2] == "d" and comando[3] == "8":
+                mcp.output(8, 0)
+            elif comando[2] == "d" and comando[3] == "9":
+                mcp.output(9, 0)
+            else:
+                print "Comando invalido!"
+     
 except KeyboardInterrupt:   #   Trata o CTRL+C
     print "Saindo..."
     exit(0)
