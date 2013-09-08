e633df89c2a13163f78e535720bd2d30a55a6f30
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-09-08 17:53:14 -0300

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -8,7 +8,7 @@ PORT = 7000             # Porta que o Servidor esta
 
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
 
-print "Configurando relês..."
+print "Configurando rees..."
 
 mcp.config(0, mcp.OUTPUT)
 mcp.config(1, mcp.OUTPUT)
