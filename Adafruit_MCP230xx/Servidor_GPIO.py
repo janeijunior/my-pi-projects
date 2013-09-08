2f1fcda864bd6017463e70ea0c981af755d1d260
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-09-08 17:54:35 -0300

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -33,7 +33,7 @@ def conectado(con, cliente):
         if not msg: 
             break
         
-        print cliente, comando
+        print cliente, " comando recebido: " + comando
                   
         
         if len(comando) > 0:
