8930fdf94eb47ad3ca26c486867af80357d1c368
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-09-08 17:50:30 -0300

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -31,6 +31,7 @@ def conectado(con, cliente):
  
         if len(comando) > 0:
             #print "Mensagem recebida -> " + msg.strip()
+            
             if comando[2] == "l" and comando[3] == "0":
                 mcp.output(0, 1)
             elif comando[2] == "l" and comando[3] == "1":
