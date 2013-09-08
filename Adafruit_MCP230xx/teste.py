12f98dec549fc9b9d2055a45e17ccd8605ffd5fa
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-09-08 17:46:45 -0300

    Modified teste.py

--- Adafruit_MCP230xx/teste.py
+++ Adafruit_MCP230xx/teste.py
@@ -4,6 +4,20 @@ import thread
 HOST = '192.168.1.104'              # Endereco IP do Servidor
 PORT = 7000            # Porta que o Servidor esta
 
+mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
+    
+mcp.config(0, mcp.OUTPUT)
+mcp.config(1, mcp.OUTPUT)
+mcp.config(2, mcp.OUTPUT)
+mcp.config(3, mcp.OUTPUT)
+mcp.config(4, mcp.OUTPUT)
+mcp.config(5, mcp.OUTPUT)
+mcp.config(6, mcp.OUTPUT)
+mcp.config(7, mcp.OUTPUT)
+mcp.config(8, mcp.OUTPUT)
+mcp.config(9, mcp.OUTPUT)
+
+
 def conectado(con, cliente):
     print 'Conectado por', cliente
 
