2839a99c99d9e5abd3d91330bfd91636f96aefd6
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-09-08 17:35:00 -0300

    Modified servidor2.py

--- Adafruit_MCP230xx/servidor2.py
+++ Adafruit_MCP230xx/servidor2.py
@@ -30,62 +30,57 @@ class verificaMensagensClientes(Thread):
         mcp.config(9, mcp.OUTPUT)
 
         
-        while (conectados > 0):
-            data = self.conn.recv(1024)
-            print data
-            for conexao in listaConectados:
-                if(conexao != self.conn):
-                    conexao.send(data)
-                    msg = conexao.recv(1024)
-          
-                    comando = msg.strip() 
-             
-                    if len(comando) > 0:
-                        print "Mensagem recebida de -> " + msg.strip()
-                        
-                        if comando[2] == "l" and comando[3] == "0":
-                            mcp.output(0, 1)
-                        elif comando[2] == "l" and comando[3] == "1":
-                            mcp.output(1, 1)
-                        elif comando[2] == "l" and comando[3] == "2":
-                            mcp.output(2, 1)
-                        elif comando[2] == "l" and comando[3] == "3":
-                            mcp.output(3, 1)
-                        elif comando[2] == "l" and comando[3] == "4":
-                            mcp.output(4, 1)
-                        elif comando[2] == "l" and comando[3] == "5":
-                            mcp.output(5, 1)
-                        elif comando[2] == "l" and comando[3] == "6":
-                            mcp.output(6, 1)
-                        elif comando[2] == "l" and comando[3] == "7":
-                            mcp.output(7, 1)
-                        elif comando[2] == "l" and comando[3] == "8":
-                            mcp.output(8, 1)
-                        elif comando[2] == "l" and comando[3] == "9":
-                            mcp.output(9, 1)
-                        elif comando[2] == "d" and comando[3] == "0":
-                            mcp.output(0, 0)
-                        elif comando[2] == "d" and comando[3] == "1":
-                            mcp.output(1, 0)
-                        elif comando[2] == "d" and comando[3] == "2":
-                            mcp.output(2, 0)
-                        elif comando[2] == "d" and comando[3] == "3":
-                            mcp.output(3, 0)
-                        elif comando[2] == "d" and comando[3] == "4":
-                            mcp.output(4, 0)
-                        elif comando[2] == "d" and comando[3] == "5":
-                            mcp.output(5, 0)
-                        elif comando[2] == "d" and comando[3] == "6":
-                            mcp.output(6, 0)
-                        elif comando[2] == "d" and comando[3] == "7":
-                            mcp.output(7, 0)
-                        elif comando[2] == "d" and comando[3] == "8":
-                            mcp.output(8, 0)
-                        elif comando[2] == "d" and comando[3] == "9":
-                            mcp.output(9, 0)
-                        else:
-                            print "Comando invalido!"
-        
+        while (1):
+            msg = conexao.recv(1024)
+    
+            comando = msg.strip() 
+     
+            if len(comando) > 0:
+                print "Mensagem recebida de -> " + msg.strip()
+                
+                if comando[2] == "l" and comando[3] == "0":
+                    mcp.output(0, 1)
+                elif comando[2] == "l" and comando[3] == "1":
+                    mcp.output(1, 1)
+                elif comando[2] == "l" and comando[3] == "2":
+                    mcp.output(2, 1)
+                elif comando[2] == "l" and comando[3] == "3":
+                    mcp.output(3, 1)
+                elif comando[2] == "l" and comando[3] == "4":
+                    mcp.output(4, 1)
+                elif comando[2] == "l" and comando[3] == "5":
+                    mcp.output(5, 1)
+                elif comando[2] == "l" and comando[3] == "6":
+                    mcp.output(6, 1)
+                elif comando[2] == "l" and comando[3] == "7":
+                    mcp.output(7, 1)
+                elif comando[2] == "l" and comando[3] == "8":
+                    mcp.output(8, 1)
+                elif comando[2] == "l" and comando[3] == "9":
+                    mcp.output(9, 1)
+                elif comando[2] == "d" and comando[3] == "0":
+                    mcp.output(0, 0)
+                elif comando[2] == "d" and comando[3] == "1":
+                    mcp.output(1, 0)
+                elif comando[2] == "d" and comando[3] == "2":
+                    mcp.output(2, 0)
+                elif comando[2] == "d" and comando[3] == "3":
+                    mcp.output(3, 0)
+                elif comando[2] == "d" and comando[3] == "4":
+                    mcp.output(4, 0)
+                elif comando[2] == "d" and comando[3] == "5":
+                    mcp.output(5, 0)
+                elif comando[2] == "d" and comando[3] == "6":
+                    mcp.output(6, 0)
+                elif comando[2] == "d" and comando[3] == "7":
+                    mcp.output(7, 0)
+                elif comando[2] == "d" and comando[3] == "8":
+                    mcp.output(8, 0)
+                elif comando[2] == "d" and comando[3] == "9":
+                    mcp.output(9, 0)
+                else:
+                    print "Comando invalido!"
+
 tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_server_socket.bind((HOST, PORT))
 tcp_server_socket.listen(2)
