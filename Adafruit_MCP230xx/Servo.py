e3da7154890d6ad6c415162fe1319581cfebf7d6
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-10-11 20:10:42 -0300

    Modified Servo.py

--- Adafruit_MCP230xx/Servo.py
+++ Adafruit_MCP230xx/Servo.py
@@ -2,11 +2,11 @@
 import time
 def set(property, value):
  try:
- f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
- f.write(value)
- f.close() 
+   f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
+   f.write(value)
+   f.close() 
  except:
- print("Error writing to: " + property + " value: " + value)
+   print("Error writing to: " + property + " value: " + value)
 def setServo(angle):
  set("servo", str(angle))
  
@@ -18,8 +18,8 @@ set("active", "1")
 delay_period = 0.01
 while True:
  for angle in range(0, 180):
- setServo(angle)
- time.sleep(delay_period)
+   setServo(angle)
+   time.sleep(delay_period)
  for angle in range(0, 180):
- setServo(180 - angle)
- time.sleep(delay_period)
+   setServo(180 - angle)
+   time.sleep(delay_period)
