23227f03b30b6a0596be15a9bce69e2f8c7872a8
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-10-27 14:36:00 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -149,6 +149,8 @@ def conectado(con, cliente):
                 mcp.output(8, 0)
             elif comando[2] == "d" and comando[3] == "9":
                 mcp.output(9, 0)
+            elif comando[2] == "d" and comando[3] == "p": 
+                mcp.output(9, 10)
             elif comando[2] == "d" and comando[3] == "a": # Desliga o alarme
                 thread1.stop()
             #else:
