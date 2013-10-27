a08db3b218769182dd80a7b97ca4f78bf4308a4a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-10-27 14:56:39 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -5,8 +5,8 @@ import time
 import Adafruit_MCP230xx
 import RPi.GPIO as GPIO 
 
-HOST = '192.168.0.104'  # IP do Servidor
-PORT = 7001             # Porta do Servidor
+HOST = '192.168.5.125'  # IP do Servidor
+PORT = 5000             # Porta do Servidor
 
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
 
