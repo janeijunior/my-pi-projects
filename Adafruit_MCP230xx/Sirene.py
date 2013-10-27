8412afb934474dda2ed78cfb1b0576ea9423eeb3
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-10-27 09:01:13 -0200

    Modified Sirene.py

--- Adafruit_MCP230xx/Sirene.py
+++ Adafruit_MCP230xx/Sirene.py
@@ -12,22 +12,9 @@ if __name__ == '__main__':
     mcp = Adafruit_MCP230XX(busnum = 1, address = 0x20, num_gpios = 16)
     # Use busnum = 1 for new Raspberry Pi's (512MB) 
     # Set pins 10, 11 and 12 to output (you can set pins 0..15 this way) 
-    mcp.config(0, OUTPUT)
-    mcp.config(1, OUTPUT)
-    mcp.config(2, OUTPUT)
-    mcp.config(3, OUTPUT)
-    mcp.config(4, OUTPUT)
-    mcp.config(5, OUTPUT)
-    mcp.config(6, OUTPUT)
-    mcp.config(7, OUTPUT)
-    mcp.config(8, OUTPUT)
-    mcp.config(9, OUTPUT)
     mcp.config(10, OUTPUT)
     mcp.config(11, OUTPUT) 
     mcp.config(12, OUTPUT)
-    mcp.config(13, OUTPUT)
-    mcp.config(14, OUTPUT)
-    mcp.config(15, OUTPUT)
     # Python speed test on output 0 toggling at max speed 
     
     #while (True): 
