8ade7bc4a82ca4855f009ba7d8a82b2883a6ef94
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-10-27 13:22:22 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -2,6 +2,7 @@ import socket
 import thread
 import time
 import Adafruit_MCP230xx
+import RPi.GPIO as GPIO 
 
 HOST = '192.168.0.104'  # IP do Servidor
 PORT = 7000             # Porta do Servidor
@@ -21,6 +22,20 @@ mcp.config(7, mcp.OUTPUT)
 mcp.config(8, mcp.OUTPUT)
 mcp.config(9, mcp.OUTPUT)  #Cooler
 mcp.config(10, mcp.OUTPUT) #Sirene Alarme
+
+print "Configurando sendores do alarme..."
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(17,GPIO.IN) #GPIO0 
+GPIO.setup(18,GPIO.IN) #GPIO1 
+GPIO.setup(27,GPIO.IN) #GPIO2 
+GPIO.setup(22,GPIO.IN) #GPIO3 
+GPIO.setup(23,GPIO.IN) #gpio4 
+GPIO.setup(24,GPIO.IN) #GPIO5 
+GPIO.setup(25,GPIO.IN) #GPIO6 
+GPIO.setup(4,GPIO.IN) #GPIO7 
+
+prev_input0 = 0 
 
 print "Aguardando conexoes..."
 
@@ -61,8 +76,7 @@ def conectado(con, cliente):
             elif comando[2] == "l" and comando[3] == "9":
                 mcp.output(9, 1)
             elif comando[2] == "l" and comando[3] == "a": # Liga o Alarme
-                
-                
+                thread.start_new_thread(Alarme)
             elif comando[2] == "d" and comando[3] == "0":
                 mcp.output(0, 0)
             elif comando[2] == "d" and comando[3] == "1":
@@ -89,6 +103,47 @@ def conectado(con, cliente):
             #else:
             #    print "Comando invalido!"
         
+def Alarme:
+    while True: 
+        #take a reading 
+        #print("Lendo sensores...")
+        
+        input0 = GPIO.input(17) 
+        input1 = GPIO.input(18) 
+        input2 = GPIO.input(27) 
+        input3 = GPIO.input(22) 
+        input4 = GPIO.input(23) 
+        input5 = GPIO.input(24) 
+        input6 = GPIO.input(25) 
+        input7 = GPIO.input(4)
+        
+        #if the last reading was low and this one high, print 
+        if (input0 == 1): 
+            print("sensor 0 Normal")
+        else:
+            print("Sendor 0 Violado!")
+            mcp.output(10, 1)
+            time.sleep(5) 
+            mcp.output(10, 0)
+            
+        if (input1 == 1): 
+            print("sensor1") 
+        if (input2 == 1): 
+            print("sensor2") 
+        if (input3 == 1): 
+            print("sensor3") 
+        if (input4 == 1):
+            print("sensor4") 
+        if (input5 == 1): 
+            print("sensor5") 
+        if (input6 == 1): 
+            print("sensor6") 
+        if (input7 == 1): 
+            print("sensor7")
+        #slight pause to debounce 
+        time.sleep(0.05)
+    
+
 
     print 'Finalizando conexao do cliente', cliente
     con.close()
