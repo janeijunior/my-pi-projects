9340b1d123c3af5c047fce27778a4b3f10221a44
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-10-27 08:59:58 -0200

    Modified Sirene.py

--- Adafruit_MCP230xx/Sirene.py
+++ Adafruit_MCP230xx/Sirene.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     mcp.output(9, 0) 
     mcp.output(10, 0) # Pin 10 High
     mcp.output(11, 0) # Pin 11 High 
-    mcp.output(12, 0)
+    mcp.output(12, 0) # Pin 12 High
     mcp.output(13, 0)
     mcp.output(14, 0)
     mcp.output(15, 0)
