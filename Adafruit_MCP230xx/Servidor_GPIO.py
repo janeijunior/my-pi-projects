18dc07c59176435dc5b95091467e46b0aa735ccc
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-09 17:59:48 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -134,7 +134,7 @@ def conectado(con, cliente):
                 thread1 = ThreadAlarme(1, "Thread-1", 1)
                 thread1.start()
             elif comando[2] == "l" and comando[3] == "c":
-                                
+                procs = commands.getoutput('ps -ef | grep ./gst-server.sh')                 
             elif comando[2] == "d" and comando[3] == "0":
                 mcp.output(0, 0)
             elif comando[2] == "d" and comando[3] == "1":
