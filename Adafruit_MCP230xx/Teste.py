6bd340d31148e3985bb5391809005ff1c32b764b
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-09 18:18:46 -0200

    Modified Teste.py

--- Adafruit_MCP230xx/Teste.py
+++ Adafruit_MCP230xx/Teste.py
@@ -1,5 +1,7 @@
 import subprocess
+import os
 
 if __name__ == '__main__':
     #var = os.system("ls")
-    subprocess.call(['./gst-server.sh'], shell=True)
+    #subprocess.call(['./gst-server.sh'], shell=True)
+    os.system('./gst-server.sh')
