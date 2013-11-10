1aaa49d458139ba5b8db2560628b0df765d87075
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-10 19:47:23 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -134,9 +134,7 @@ def conectado(con, cliente):
                 thread1 = ThreadAlarme(1, "Thread-1", 1)
                 thread1.start()
             elif comando[2] == "l" and comando[3] == "c":
-                os.system('cd /root/mjpg-streamer')
-                os.system('ls')
-                os.system('sudo ./mjpg-streamer.sh start')                
+                os.system('mjpg-streamer/mjpg-streamer.sh start')                
             elif comando[2] == "d" and comando[3] == "0":
                 mcp.output(0, 0)
             elif comando[2] == "d" and comando[3] == "1":
