60a5977d76661bfaaa431836bf1949007b410c0d
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-13 19:54:39 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -42,19 +42,19 @@ def ConfigurarRelesLeitura():
     mcp.config(9, mcp.INPUT)  #Cooler
 
 def PegarXMLStatusRele():
-  doc = xml.dom.minidom.Document()
-                root = doc.createElement('Status')
-                rele = doc.createElement('Rele')
-                
-                ConfigurarRelesLeitura()
-                
-                if mcp.input(0) == 1:
-                    rele.setAttribute('rele1', '1')
-                else:
-                    rele.setAttribute('rele1', '0')
-                
-                doc.appendChild(root)
-                root.appendChild(rele)
+    doc = xml.dom.minidom.Document()
+    root = doc.createElement('Status')
+    rele = doc.createElement('Rele')
+    
+    ConfigurarRelesLeitura()
+    
+    if mcp.input(0) == 1:
+        rele.setAttribute('rele1', '1')
+    else:
+        rele.setAttribute('rele1', '0')
+    
+    doc.appendChild(root)
+    root.appendChild(rele)
 
 mcp.config(10, mcp.OUTPUT) #Sirene Alarme
 
