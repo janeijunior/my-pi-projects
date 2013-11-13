117ce9b9ec9905770019092dd7ce5bb92f457ee3
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-13 20:06:15 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -9,7 +9,7 @@ import commands
 import xml.dom.minidom
 
 HOST = '192.168.5.125'  # IP do Servidor
-PORT = 5001            # Porta do Servidor
+PORT = 5000            # Porta do Servidor
 
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
 
