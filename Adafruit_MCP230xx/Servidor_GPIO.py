941f4f3f43202c4d0e106d98a6470a3c9a6cb356
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-16 07:59:35 -0200

    Modified Servidor_GPIO.py

--- Adafruit_MCP230xx/Servidor_GPIO.py
+++ Adafruit_MCP230xx/Servidor_GPIO.py
@@ -8,7 +8,7 @@ import os
 import commands
 import xml.dom.minidom
 
-HOST = socket.gethostbyname(socket.gethostname())  # IP do Servidor
+HOST = '192.168.1.20'  # IP do Servidor
 PORT = 5000            # Porta do Servidor
 
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
