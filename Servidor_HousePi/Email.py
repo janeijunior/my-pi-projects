23b9d20d44a70f11d3b7d274a3759bca9a714a1a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-17 19:09:31 -0200

    Modified Email.py

--- Servidor_HousePi/Email.py
+++ Servidor_HousePi/Email.py
@@ -15,18 +15,18 @@ esenha = 'britsp3021281ney'
 econteudo = 'E-mail enviado automaticamento pelo aplicativo House Pi'
 
 print 'enviando email\n'
-    try:
-        msg1 = MIMEText('%s'% econteudo)
-        msg1['Subject'] = econteudo
-        msg1['From'] = efrom
-        msg1['To'] = eto
-        serv=smtplib.SMTP(eservidor,587)
-        serv.ehlo()
-        serv.starttls()
-        serv.login(efrom,esenha)
-        serv.sendmail(msg1['From'], msg1['To'], msg1.as_string())
-        serv.quit()
-    except Exception, e:
-        print "Erro ",e
-    else:
-        print "Enviado!"
+try:
+    msg1 = MIMEText('%s'% econteudo)
+    msg1['Subject'] = econteudo
+    msg1['From'] = efrom
+    msg1['To'] = eto
+    serv=smtplib.SMTP(eservidor,587)
+    serv.ehlo()
+    serv.starttls()
+    serv.login(efrom,esenha)
+    serv.sendmail(msg1['From'], msg1['To'], msg1.as_string())
+    serv.quit()
+except Exception, e:
+    print "Erro ",e
+else:
+    print "Enviado!"
