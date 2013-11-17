e21d4af73d40de3086a40474b2f1c0989d51ac3c
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-17 19:55:50 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -90,7 +90,7 @@ def conectado(con, cliente):
             elif comando[2] == "l" and comando[3] == "p":
                 mcp.output(10, 1)
             elif comando[2] == "l" and comando[3] == "a": # Liga o Alarme
-                threadalarme = ThreadAlarme.ThreadAlarme(1, "Thread", 1)
+                threadalarme = ThreadAlarme.ThreadAlarme(1, "Thread", 1,  mcp)
                 threadalarme.start()
             elif comando[2] == "l" and comando[3] == "c":
                 os.system('mjpg-streamer/mjpg-streamer.sh start')
