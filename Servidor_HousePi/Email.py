6b7c2841fd263c536ea523567bd260441b015126
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-17 19:22:15 -0200

    Modified Email.py

--- Servidor_HousePi/Email.py
+++ Servidor_HousePi/Email.py
@@ -8,20 +8,23 @@ from email.MIMEText import MIMEText
 def EnviarEmail():
     form = cgi.FieldStorage()
     
-    efrom = 'robatistello@gmail.com'
-    eto = 'rodrigobatistello@hotmail.com'
-    esubject = 'Alarme disparado!'
-    eservidor = 'smtp.gmail.com'
-    esenha = 'britsp3021281ney'
-    econteudo = 'E-mail enviado automaticamento pelo aplicativo House Pi'
+    remetente = 'robatistello@gmail.com'
+    destinatario = 'rodrigobatistello@hotmail.com'
+    assunto = 'Alarme disparado!'
+    servidor = 'smtp.gmail.com'
+    porta = 587
+    senha = 'britsp3021281ney'
+    conteudo = 'E-mail enviado automaticamento pelo aplicativo House Pi'
     
     print 'Enviando e-mail\n'
     try:
-        msg1 = MIMEText('%s'% econteudo)
-        msg1['Subject'] = esubject
-        msg1['From'] = efrom
-        msg1['To'] = eto
-        serv=smtplib.SMTP(eservidor,587)
+        msg = MIMEText('%s'% conteudo)
+        msg['Subject'] = assunto
+        msg['From'] = remetente
+        msg['To'] = destinatario
+        
+        smtp = smtplib.SMTP(servidor, porta)
+        
         serv.ehlo()
         serv.starttls()
         serv.login(efrom,esenha)
