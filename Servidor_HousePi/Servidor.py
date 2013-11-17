7b97351291ca051296595101dbbe11ce5b6eea5d
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-17 19:48:19 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -135,21 +135,3 @@ def conectado(con, cliente):
     print 'Finalizando conexao do cliente', cliente
     con.close()
     thread.exit()
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-orig = (HOST, PORT)
-
-tcp.bind(orig)
-tcp.listen(1)
-
-ConfigurarReles()
-ConfigurarSensoresAlarme()
-
-print "Aguardando conexoes..."
-
-while True:
-   con, cliente = tcp.accept()
-   thread.start_new_thread(conectado, tuple([con, cliente]))
-
-tcp.close()
