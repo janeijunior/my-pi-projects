3ca140c7de4ba51020189fb6521c7181161b16b7
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-11-23 08:12:07 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -9,7 +9,7 @@ import commands
 import xml.dom.minidom
 import ThreadAlarme
 
-HOST = '192.168.1.20'    # IP do Servidor
+HOST = ''    # IP do Servidor
 PORT = 5000  # Porta do Servidor
 
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
