34cd46a5a5099d6c97c676c9e404c59df2c40ca3
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-05 21:16:50 -0200

    Modified Rele.py

--- Servidor_HousePi/Rele.py
+++ Servidor_HousePi/Rele.py
@@ -1,21 +1,20 @@
 class Rele(object):
     
+    #construtor
     def __init__(self, numero, status, nome):
         self.numero = numero
         self.status = status
         self.nome = nome
         
-    def get_velocidade(self):
-        return self.alcance / self.tempo
-    
+    #Propriedades
     @property
-    def numero(self):
+    def getNumero(self):
         return self.numero
         
     @property
-    def status(self):
+    def getStatus(self):
         return self.status
     
     @property
-    def nome(self):
+    def getNome(self):
         return self.nome
