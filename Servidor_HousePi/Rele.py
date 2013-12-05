0ba97dc59e7fd97934f745bf10cfa55cc0a256c9
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-05 21:16:04 -0200

    Modified Rele.py

---
+++ Servidor_HousePi/Rele.py
@@ -0,0 +1,22 @@
+class Rele(object):
+    
+    def __init__(self, numero, status, nome):
+        self.numero = numero
+        self.status = status
+        self.nome = nome
+        
+    def get_velocidade(self):
+        return self.alcance / self.tempo
+    
+    
+    @property
+    def numero(self):
+        return self.numero
+        
+    @property
+    def status(self):
+        return self.status
+    
+    @property
+    def nome(self):
+        return self.nome
