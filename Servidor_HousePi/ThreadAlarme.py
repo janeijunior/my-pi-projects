0b59d26d83b1acb44a920c3b3bba1d0ede923931
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-05 21:56:31 -0200

    Modified ThreadAlarme.py

--- Servidor_HousePi/ThreadAlarme.py
+++ Servidor_HousePi/ThreadAlarme.py
@@ -53,7 +53,3 @@ class ThreadAlarme(threading.Thread):
                 print("sensor7")
             
             time.sleep(0.05)
-    
-    @property
-    def setMcp(self, aMcp):
-        self.mcp = aMcp
