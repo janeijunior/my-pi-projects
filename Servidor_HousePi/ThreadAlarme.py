f34d75808865254aa6d5cf6814b0cfdf78498446
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-06 20:34:32 -0200

    Modified ThreadAlarme.py

--- Servidor_HousePi/ThreadAlarme.py
+++ Servidor_HousePi/ThreadAlarme.py
@@ -10,7 +10,7 @@ import Adafruit_MCP230xx
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
 
 class ThreadAlarme(threading.Thread):
-    def __init__(self, threadID, name, counter, tempoDisparo):
+    def __init__(self, tempoDisparo):
         threading.Thread.__init__(self)
         self.threadID = 1
         self.name = 'ThreadAlarme'
