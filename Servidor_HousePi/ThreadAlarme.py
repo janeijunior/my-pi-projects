b121155526219977b386389b014a8a071e81fbbb
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-06 21:16:57 -0200

    Modified ThreadAlarme.py

--- Servidor_HousePi/ThreadAlarme.py
+++ Servidor_HousePi/ThreadAlarme.py
@@ -28,14 +28,14 @@ class ThreadAlarme(threading.Thread):
         
         listaSensores = [];
         
-        listaSensores.insert(0, SensorAlarme.SensorAlarme(numero = 17, ativo =  True, nome = "Casa 0")) #GPIO 0 
-        listaSensores.insert(1, SensorAlarme.SensorAlarme(numero = 18, ativo = False, nome = "Casa 1")) #GPIO 1
-        listaSensores.insert(2, SensorAlarme.SensorAlarme(numero = 27, ativo = False, nome = "Casa 2")) #GPIO 2
-        listaSensores.insert(3, SensorAlarme.SensorAlarme(numero = 22, ativo = False, nome = "Casa 3")) #GPIO 3
-        listaSensores.insert(4, SensorAlarme.SensorAlarme(numero = 23, ativo = False, nome = "Casa 4")) #GPIO 4
-        listaSensores.insert(5, SensorAlarme.SensorAlarme(numero = 24, ativo = False, nome = "Casa 5")) #GPIO 5
-        listaSensores.insert(6, SensorAlarme.SensorAlarme(numero = 25, ativo = False, nome = "Casa 6")) #GPIO 6
-        listaSensores.insert(7, SensorAlarme.SensorAlarme(numero =  4, ativo = False, nome = "Casa 7"))  #GPIO 7
+        listaSensores.insert(0, SensorAlarme.SensorAlarme(numero = 17, ativo = 1, nome = "Casa 0")) #GPIO 0 
+        listaSensores.insert(1, SensorAlarme.SensorAlarme(numero = 18, ativo = 0, nome = "Casa 1")) #GPIO 1
+        listaSensores.insert(2, SensorAlarme.SensorAlarme(numero = 27, ativo = 0, nome = "Casa 2")) #GPIO 2
+        listaSensores.insert(3, SensorAlarme.SensorAlarme(numero = 22, ativo = 0, nome = "Casa 3")) #GPIO 3
+        listaSensores.insert(4, SensorAlarme.SensorAlarme(numero = 23, ativo = 0, nome = "Casa 4")) #GPIO 4
+        listaSensores.insert(5, SensorAlarme.SensorAlarme(numero = 24, ativo = 0, nome = "Casa 5")) #GPIO 5
+        listaSensores.insert(6, SensorAlarme.SensorAlarme(numero = 25, ativo = 0, nome = "Casa 6")) #GPIO 6
+        listaSensores.insert(7, SensorAlarme.SensorAlarme(numero =  4, ativo = 0, nome = "Casa 7"))  #GPIO 7
                 
         rele = Rele.Rele(numero = 10, status = 0, nome = 'Sirene')
         
