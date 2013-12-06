c159d0bd9e6625bae780bbd8c7d237dbd0a8c7ef
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-05 22:35:20 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -12,7 +12,7 @@ import MySQLdb
 import Rele
 
 HOST = ''    # IP do Servidor (em branco = atual)
-PORT = 5000  # Porta do Servidor
+PORT = 5001  # Porta do Servidor
 
 #Variavel para controle dos pinos GPIO (reles)
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
