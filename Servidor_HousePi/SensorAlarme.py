bb42459d7c2ae957bb9d7625584c0ca5fcc21247
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-06 19:59:59 -0200

    Modified SensorAlarme.py

---
+++ Servidor_HousePi/SensorAlarme.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO 
+
+#Variavel para controle dos pinos GPIO (sensores)
+
+class Rele(object):
+    
+    #construtor
+    def __init__(self, numero, status, nome):
+        self.numero = numero
+        self.status = status
+        self.nome = nome
+        
+    #propriedades
+    @property
+    def getNumero(self):
+        return self.numero
+        
+    @property
+    def getStatus(self):
+        return self.status
+    
+    @property
+    def getNome(self):
+        return self.nome
+    
+    @property
+    def setNumero(self, numero):
+        self.numero = numero
+    
+    @property
+    def setStatus(self, status):
+        self.status = status
+    
+    @property
+    def setNome(self, nome):
+        self.nome = nome
+    
+    #funcoes
+    def configurar(self):
+        mcp.config(self.numero, mcp.OUTPUT)
+    
+    def ligar(self):
+        mcp.output(self.numero, 1)
+    
+    def desligar(self):
+        mcp.output(self.numero, 0)
+    
+    #destrutor
+    #def __done__(self):
