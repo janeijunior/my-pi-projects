a9c34b7bd69ae783f800dbd88e56f42de8e6c0b2
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-06 21:09:43 -0200

    Modified ThreadAlarme.py

--- Servidor_HousePi/ThreadAlarme.py
+++ Servidor_HousePi/ThreadAlarme.py
@@ -12,7 +12,7 @@ import SensorAlarme
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
 
 class ThreadAlarme(threading.Thread):
-    def __init__(self, tempoDisparo):
+    def __init__(self, tempoDisparo, usarSirene, enviarEmail):
         threading.Thread.__init__(self)
         self.threadID = 1
         self.name = 'ThreadAlarme'
