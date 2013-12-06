82431ff06758396cf2cd7cdd8d26eb8ff6f4b874
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-06 20:03:35 -0200

    Modified EnviaEmail.py

--- Servidor_HousePi/EnviaEmail.py
+++ Servidor_HousePi/EnviaEmail.py
@@ -1,36 +1,37 @@
-import cgi, cgitb
-import smtplib
-import sys
-import commands
+import RPi.GPIO as GPIO 
 
-from email.MIMEText import MIMEText
+class Rele(object):
+    
+    #construtor
+    def __init__(self, numero, nome):
+        self.numero = numero
+        self.ativo = ativo
+        
+        configurar()
+        
+    #propriedades
+    @property
+    def getNumero(self):
+        return self.numero
+    
+    @property
+    def getAtivo(self):
+        return self.ativo
+    
+    @property
+    def setNumero(self, numero):
+        self.numero = numero
 
-def EnviarEmail():
-    form = cgi.FieldStorage()
+    @property
+    def setAtivo(self, ativo):
+        self.ativo = ativo
     
-    remetente = 'robatistello@gmail.com'
-    destinatario = 'rodrigobatistello@hotmail.com'
-    assunto = 'Alarme disparado!'
-    servidor = 'smtp.gmail.com'
-    porta = 587
-    senha = '#brit267930ney#'
-    conteudo = 'E-mail enviado automaticamento pelo aplicativo House Pi'
+    #funcoes
+    def configurar(self):
+        GPIO.setmode(GPIO.BCM) 
+        
+    def lerStatus(self):
+        mcp.output(self.numero, 1)
     
-    print 'Enviando e-mail\n'
-    try:
-        msg = MIMEText('%s'% conteudo)
-        msg['Subject'] = assunto
-        msg['From'] = remetente
-        msg['To'] = destinatario
-        
-        smtp = smtplib.SMTP(servidor, porta)
-        
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.login(remetente, senha)
-        smtp.sendmail(msg['From'], msg['To'], msg.as_string())
-        smtp.quit()
-    except Exception, e:
-        print "Erro: ",e
-    else:
-        print "Enviado!"
+    #destrutor
+    #def __done__(self):
