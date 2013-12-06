4604c7d5d73d81e0a78c6e62648f33221563cc2c
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-06 20:52:25 -0200

    Modified ThreadAlarme.py

--- Servidor_HousePi/ThreadAlarme.py
+++ Servidor_HousePi/ThreadAlarme.py
@@ -42,9 +42,9 @@ class ThreadAlarme(threading.Thread):
             for i in range(0, 7): 
                 if listaSensores[i].getAtivo == 1:
                     if listaSensores[i].lerStatus() == 1:
-                        print("Sensor: " + str(i) + " - " + listaSensores[i].getNome + " normal")
+                        print("Sensor: " + str(i) + " - " + listaSensores[i].getNome + " normal.")
                     else:
-                        print("Sensor: " + str(i) + " - " + listaSensores[i].getNome + " violado!")
+                        print("Sensor: " + str(i) + " - " + listaSensores[i].getNome + " violado.")
                         #rele.ligar()
                         #rele.desligar()
                         #EnviaEmail.EnviarEmail()
