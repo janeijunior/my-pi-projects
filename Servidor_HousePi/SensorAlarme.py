b2858edf68669d174e38ead171ed4788deb6b96e
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-07 11:44:46 -0200

    Modified SensorAlarme.py

--- Servidor_HousePi/SensorAlarme.py
+++ Servidor_HousePi/SensorAlarme.py
@@ -14,31 +14,6 @@ class SensorAlarme(object):
         
         self.configurar()
             
-    #propriedades
-    @property
-    def getNumero(self):
-        return self.numero
-    
-    @property
-    def getAtivo(self):
-        return self.ativo
-    
-    @property
-    def getNome(self):
-        return self.nome
-        
-    @property
-    def setNumero(self, numero):
-        self.numero = numero
-
-    @property
-    def setAtivo(self, ativo):
-        self.ativo = ativo 
-    
-    @property
-    def setNome(self, nome):
-        self.nome = nome
-    
     #funcoes
     def configurar(self):
         GPIO.setmode(GPIO.BCM) 
