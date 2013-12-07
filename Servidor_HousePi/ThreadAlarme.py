002dbadafab25bee6510454583a5a3a2e1e15758
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-07 11:47:03 -0200

    Modified ThreadAlarme.py

--- Servidor_HousePi/ThreadAlarme.py
+++ Servidor_HousePi/ThreadAlarme.py
@@ -9,6 +9,7 @@ import Adafruit_MCP230xx
 import Rele
 import SensorAlarme
 
+#variavel para acionamento da sirene
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
 
 class ThreadAlarme(threading.Thread):
