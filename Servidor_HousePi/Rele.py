04265344bda97d63c170f1d285ac64ce4fb22bd6
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-07 11:42:48 -0200

    Modified Rele.py

--- Servidor_HousePi/Rele.py
+++ Servidor_HousePi/Rele.py
@@ -2,7 +2,7 @@
 
 import Adafruit_MCP230xx
 
-#Variavel para controle dos pinos GPIO (reles)
+#variavel para controle dos pinos GPIO (reles)
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
 
 class Rele(object):
@@ -10,7 +10,7 @@ class Rele(object):
     #construtor
     def __init__(self, numero, status, nome):
         
-        #Atributos publicos da classe
+        #atributos publicos da classe
         self.numero = numero
         self.status = status
         self.nome   = nome
