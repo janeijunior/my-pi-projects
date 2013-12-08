d6e8088084e12eb442ab887b9e35b9eb1048ba62
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-08 14:04:06 -0200

    Modified Conexao.py

--- Servidor_HousePi/Conexao.py
+++ Servidor_HousePi/Conexao.py
@@ -8,7 +8,7 @@ class Conexao:
     #construtor
     def __init__(self, sock=None):
         if sock is None:
-            self.sock = socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         else:
             self.sock = sock
 
