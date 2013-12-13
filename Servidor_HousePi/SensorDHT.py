448aff20199eca8cf251aa5dcd78d303a1b0427d
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-13 00:34:27 -0200

    Modified SensorDHT.py

--- Servidor_HousePi/SensorDHT.py
+++ Servidor_HousePi/SensorDHT.py
@@ -6,23 +6,26 @@ import sys
 import time
 import datetime
 
-output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
+def lerSensor:
+    return = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
+
+output  = lerSensor(); 
 matches = re.search("Temp =\s+([0-9.]+)", output)
 
 if (not matches):
   time.sleep(3)
-  output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
-  matches = re.search("Temp =\s+([0-9.]+)", output)    
-
+  output  = lerSensor();
+  matches = re.search("Temp =\s+([0-9.]+)", output)
+  
 temp = float(matches.group(1))
 
 matches = re.search("Hum =\s+([0-9.]+)", output)
 
 if (not matches):
   time.sleep(3)
-  output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
-  matches = re.search("Temp =\s+([0-9.]+)", output)
-
+  output  = lerSensor();     
+  matches = re.search("Hum =\s+([0-9.]+)", output)  
+  
 humidity = float(matches.group(1))
 
 print "Temperatura: %.1f C" % temp
