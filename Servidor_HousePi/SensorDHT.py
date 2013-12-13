453c80db207b84a419ac966a08219cec6b52dc0f
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-13 00:29:30 -0200

    Modified SensorDHT.py

--- Servidor_HousePi/SensorDHT.py
+++ Servidor_HousePi/SensorDHT.py
@@ -11,14 +11,18 @@ matches = re.search("Temp =\s+([0-9.]+)", output)
 
 if (not matches):
   time.sleep(3)
-  
+  output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
+  matches = re.search("Temp =\s+([0-9.]+)", output)    
+
 temp = float(matches.group(1))
 
 matches = re.search("Hum =\s+([0-9.]+)", output)
 
 if (not matches):
   time.sleep(3)
-  
+  output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"]);
+  matches = re.search("Temp =\s+([0-9.]+)", output)
+
 humidity = float(matches.group(1))
 
 print "Temperatura: %.1f C" % temp
