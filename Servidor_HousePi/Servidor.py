209c677eebff0067f4f5d9ddabb326f2f93d101b
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 13:28:55 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -129,7 +129,7 @@ def enviarConfiguracaoStatus():
     root = Element("Reles")
     
     for i in range(0, 10):
-        print listaReles[i].nome.decode("utf-8")
+        print listaReles[i].nome.decode("latin-1")
         root.append(Element("Rele" + str(i), Status=str(listaReles[i].status), Nome=listaReles[i].nome))
     
     xmlstr = ET.tostring(root, encoding="latin-1") + "\n"   
