debc4711c2120990f75dbf5a7c2b39aa332cb53a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 19:02:52 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -55,11 +55,6 @@ def configurarReles():
 
     listaReles[12].ligar()
 
-#inicializa o alarme
-def configurarAlarme():
-    global threadAlarme
-    threadAlarme = ThreadAlarme.ThreadAlarme(conBanco = conBanco, sirene = listaReles[SIRENE])
-
 #função para validar o usuario e a senha, se nao estiverem certos desconecta!
 def efetuarLogin(root):
     cursor = conBanco.cursor(MySQLdb.cursors.DictCursor)
