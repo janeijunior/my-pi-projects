e50d0bd93f4be04b67b4b1e7e38c957d97f71f5e
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 14:06:01 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -133,7 +133,7 @@ def enviarConfiguracaoStatus():
     root = Element("Reles")
     
     for i in range(0, 10):
-        root.append(Element("Rele" + str(i), Status=str(listaReles[i].status), Nome=listaReles[i].nome))
+        root.append(Element("Rele" + str(i), Status=str(listaReles[i].status), Nome=removerAcentos(listaReles[i].nome)))
     
     xmlstr = ET.tostring(root, encoding="utf-8") + "\n"   
     print xmlstr
