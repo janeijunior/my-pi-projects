ce150ce1affddc07008e52ca08d8a0e8114edc8c
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 18:54:11 -0200

    Modified Alarme.py

--- Servidor_HousePi/Alarme.py
+++ Servidor_HousePi/Alarme.py
@@ -13,9 +13,11 @@ class Rele(object):
     #funcoes
     #funcao para ligar o alarme
     def ligarAlarme(self):
+        self.alarmeLigado = True
         
     #funcao para desligar o alarme
     def desligarAlarme(self):
+        self.alarmeLigado = True
         
     #funcao para ligar o panico do alarme
     def ligarPanicoAlarme(self):
