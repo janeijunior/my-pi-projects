9b96217a56f14d4be67206f20248a052fd0c556c
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 19:16:37 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -149,7 +149,7 @@ def enviarConfiguracaoStatusAlarme():
     global alarme
     
     root.append(Element("SensorAlarme", Status=str(alarme.getStatusAlarme()), Ligado=str(int(alarme.alarmeLigado))))
-    root.append(Element("PanicoAlarme", Ligado=str(int(panicoAlarme))))
+    root.append(Element("PanicoAlarme", Ligado=str(int(alarme.panicoAlarmeLigado))))
     
     xmlstr = ET.tostring(root) + "\n"   
     print xmlstr
