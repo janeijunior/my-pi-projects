10a82d9566751c465c525c99f8b66cc0e7bbefcc
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 15:42:44 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -150,7 +150,7 @@ def enviarConfiguracaoStatus():
 def enviarConfiguracaoStatus():
     root = Element("Alarme")
     
-    root.append(Element("SensorAlarme", Status=str(threadAlarme.status), Ligado=str(threadAlarme.ligado)))
+    root.append(Element("SensorAlarme", Status=str(threadAlarme.status), Ligado=str(int(threadAlarme.ligado))))
     root.append(Element("PanicoAlarme", Ligado=str(int(panicoLigado))))
     
     xmlstr = ET.tostring(root) + "\n"   
