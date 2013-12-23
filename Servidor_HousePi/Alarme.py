d1897979c26a6fafffa13ec27d44b7aeb4b7e30f
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 18:51:19 -0200

    Modified Alarme.py

--- Servidor_HousePi/Alarme.py
+++ Servidor_HousePi/Alarme.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python
 
+class Rele(object):
+    
+    #construtor
+    def __init__(self, id, numeroGPIO, status, nome):
+        
+        #atributos publicos da classe
+        self.status = 
+        self.ligado = False
+            
+    #funcoes
+    #funcao para ligar o alarme
+    def ligarAlarme(self):
+        mcp.config(self.numeroGPIO, mcp.OUTPUT)
+    
+    #funcao para ligar o rele
+    def ligar(self):
+        mcp.output(self.numeroGPIO, LIGAR)
+        self.status = STATUS_LIGADO
+    
+    #funcao para desligar o rele
+    def desligar(self):
+        mcp.output(self.numeroGPIO, DESLIGAR)
+        self.status = STATUS_DESLIGADO
+    
+    #destrutor
+    #def __done__(self):
+    #    self.desligar()
