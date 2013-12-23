5605e584179b65579cf250ff4c5d959ed2812e03
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 19:03:52 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -58,7 +58,6 @@ def configurarReles():
 #inicializa o alarme
 def configurarAlarme():
     global alarme
-    
     alarme = Alarme.Alarme()
 
 #função para validar o usuario e a senha, se nao estiverem certos desconecta!
