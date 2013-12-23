0787cd6b1d16176731b4945c97d594d275c77ed3
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 14:06:23 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-from xml.etree.ElementTree import Element/
+from xml.etree.ElementTree import Element
 from unicodedata import normalize
 import xml.etree.ElementTree as ET
 import socket
