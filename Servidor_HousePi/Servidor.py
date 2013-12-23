064a36111004e09141d7a2d132dd20dc510cedf1
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 09:41:17 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -128,7 +128,7 @@ def enviarConfiguracaoStatus():
     root = Element("Reles")
     
     for i in range(0, 10):
-        root.append(Element("Rele" + str(i), Status=str(listaReles[i].status), str(Nome=listaReles[i].nome)))
+        root.append(Element("Rele" + str(i), Status=str(listaReles[i].status), Nome=str(listaReles[i].nome)))
     
     xmlstr = ET.tostring(root) + "\n"   
     con.send(xmlstr)
