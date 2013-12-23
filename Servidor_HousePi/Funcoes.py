5861421d84c2ad3d26e8b10fd56abb95485f9383
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-23 20:32:22 -0200

    Modified Funcoes.py

---
+++ Servidor_HousePi/Funcoes.py
@@ -0,0 +1,14 @@
+#funcao para conectar no banco de dados
+def conectarBanco():
+    HOST   = "localhost"
+    USER   = "root"
+    PASSWD = "batistello"
+    BANCO  = "HousePi"
+
+    try:
+        conBanco = MySQLdb.connect(HOST, USER, PASSWD)
+	    conBanco.select_db(BANCO)
+    except MySQLdb.Error, e:
+        print "Não foi possivel conectar ao banco de dados.", e
+
+    return conBanco
