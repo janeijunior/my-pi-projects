68840a642665b2d3d0a4b7a72c8ead51eac46e6c
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-27 19:57:30 -0200

    Modified Agendamento.py

--- Servidor_HousePi/Agendamento.py
+++ Servidor_HousePi/Agendamento.py
@@ -19,24 +19,25 @@ class Agendamento(object):
     #funcoes
     #funcao para gravar um novo agendamento no banco de dados
     def gravarRegistroBanco(self):
+    
+
+        #try:
+        conBanco = Funcoes.conectarBanco()
+        cursor = conBanco.cursor(MySQLdb.cursors.DictCursor)
         
-        try:
-            conBanco = Funcoes.conectarBanco()
-            cursor = conBanco.cursor(MySQLdb.cursors.DictCursor)
-            
-            if self.rele == None:
-                sql = "insert into Agendamento (DataHoraInicial, DataHoraFinal, EhAlarme) values ({dataInicial}, {dataFinal}, 1)".format(dataInicial = self.dataHoraInicial, dataFinal = self.dataHoraFinal)
-            else:
-                sql = "insert into Agendamento (IdRele, DataHoraInicial, DataHoraFinal, EhAlarme) values ({idRele}, {dataInicial}, {dataFinal}, 0)".format(idRele = self.rele.id, dataInicial = self.dataHoraInicial, dataFinal = self.dataHoraFinal)
-        
-            print sql
-        
-            cursor.execute(sql)
-            conBanco.commit()
-            return True
-        except:
-            conBanco.rollback()
-            return False
+        if self.rele == None:
+            sql = "insert into Agendamento (DataHoraInicial, DataHoraFinal, EhAlarme) values ({dataInicial}, {dataFinal}, 1)".format(dataInicial = self.dataHoraInicial, dataFinal = self.dataHoraFinal)
+        else:
+            sql = "insert into Agendamento (IdRele, DataHoraInicial, DataHoraFinal, EhAlarme) values ({idRele}, {dataInicial}, {dataFinal}, 0)".format(idRele = self.rele.id, dataInicial = self.dataHoraInicial, dataFinal = self.dataHoraFinal)
+    
+        print sql
+    
+        cursor.execute(sql)
+        conBanco.commit()
+        return True
+        #except:
+        #    conBanco.rollback()
+        #    return False
             
     #funcao para desativar o agendamento no banco de dados
     #def desativarRegistroBanco(self): 
