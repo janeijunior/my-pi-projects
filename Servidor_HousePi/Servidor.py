325f038c0c597c90071c682965015c858112ec34
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-27 19:59:30 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -19,7 +19,7 @@ import Funcoes
 import Agendamento
 
 HOST = ''    # IP do Servidor (em branco = IP do sistema)
-PORT = 5001  # Porta do Servidor
+PORT = 5000  # Porta do Servidor
 SIRENE = 10
 
 orig = (HOST, PORT)
