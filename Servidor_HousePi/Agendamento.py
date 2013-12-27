e61621183d5a25b110f11f9445a594c9cbac86b8
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-27 15:03:58 -0200

    Modified Agendamento.py

--- Servidor_HousePi/Agendamento.py
+++ Servidor_HousePi/Agendamento.py
@@ -15,38 +15,12 @@ class Agendamento(object):
         self.ativo = ativo
         
     #funcoes
-    #funcao para ligar o alarme
-    def ligarAlarme(self):
-        self.threadAlarme = ThreadAlarme.ThreadAlarme(sirene=self.sirene)
-        self.threadAlarme.start() 
-        self.alarmeLigado = True
+    #funcao para gravar um novo agendamento no banco de dados
+    def gravarAgendamento(self):
         
-    #funcao para desligar o alarme
-    def desligarAlarme(self):
-        self.threadAlarme.stop()
-        self.alarmeLigado = False
-    
-    #funcao para ler o status do alarme
-    def getStatusAlarme(self):
-        try:
-            return self.threadAlarme.status
-        except:
-            return 0
-            print "Erro ao ler status"
-        
-    #funcao para ligar o panico do alarme
-    def ligarPanicoAlarme(self):
-        self.sirene.ligar()
-        self.panicoAlarmeLigado = True
-        
-    #funcao para desligar o panico do alarme
-    def desligarPanicoAlarme(self):
-        if self.getStatusAlarme() == 0:
-            self.sirene.desligar()
-        
-        self.panicoAlarmeLigado = False
+    #funcao para desativar o agendamento no banco de dados
+    def desativarAgendamento(self):
         
     #destrutor
-    def __done__(self):
-        self.desligarPanicoAlarme()
-        self.desligarAlarme()
+    #def __done__(self):
+        
