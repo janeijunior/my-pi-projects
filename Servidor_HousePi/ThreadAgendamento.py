7c261823c1289aa892f8fd4573cf5920e85a3953
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-28 15:20:25 -0200

    Modified ThreadAgendamento.py

--- Servidor_HousePi/ThreadAgendamento.py
+++ Servidor_HousePi/ThreadAgendamento.py
@@ -28,6 +28,7 @@ class ThreadAgendamento(threading.Thread):
         #executa enquanto nao setar o evento
         while not self.__stop_thread_event.isSet(): 
             
+            #data e hora atual
             atual = datetime.now().toordinal() 
             
             for agendamento in self.listaAgendamento
