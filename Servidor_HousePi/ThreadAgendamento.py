549e4077cfb194a44cbaed1fd64ce40347675991
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-29 10:38:24 -0200

    Modified ThreadAgendamento.py

--- Servidor_HousePi/ThreadAgendamento.py
+++ Servidor_HousePi/ThreadAgendamento.py
@@ -34,11 +34,11 @@ class ThreadAgendamento(threading.Thread):
             for agendamento in self.listaAgendamento:
                 #data e hora para ligar
                 dtLigar = datetime.strptime(str(agendamento.dataHoraInicial), "%Y-%m-%d %H:%M:%S")
-                ligar = dtLigar.strftime("%Y%m%d %H:%M:%S")
+                ligar = dtLigar.strftime("%Y%m%d%H%M%S")
                 
                 #data e hora para desligar
                 dtDesligar = datetime.strptime(str(agendamento.dataHoraFinal), "%Y-%m-%d %H:%M:%S")
-                desligar = dtDesligar.strftime("%Y%m%d %H:%M:%S")
+                desligar = dtDesligar.strftime("%Y%m%d%H%M%S")
                 
                 print "Data atual: ", str(atual)
                 print "Data ligar: ", str(ligar)
