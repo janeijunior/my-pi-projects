4b293b439d5bb242b13a8a4733647ba70323f7e3
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2013-12-31 14:57:56 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -435,7 +435,6 @@ def controlarSomAmbiente(root):
 
     if comando == "Play":
         if valor == "0":
-            #-volume {volumeEX} .format(volumeEx = root.get("Volume")) 
             executar = '''find /home/pi/HousePi/Musicas/ -name "*mp3" -o -name "*flac" -type f > ~/playlist
                           mplayer -slave -input file=/home/pi/HousePi/mplayer_control -playlist ~/playlist </dev/null >/dev/null 2>&1 &'''         
     elif comando == "Pause":
