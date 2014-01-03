0ad049405d9523e436aeb5bee82fdc134b8f12ee
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-01-03 11:40:06 -0200

    Modified Servidor.py

--- Servidor_HousePi/Servidor.py
+++ Servidor_HousePi/Servidor.py
@@ -492,15 +492,13 @@ def reiniciarDesligarServidor(root):
 	acao = root.find("Acao").text
 	
 	finalizarProcessos()
-	
-    command = ""
     
 	if acao == "Reiniciar":
 		command = "/usr/bin/sudo /sbin/shutdown -r now"
 	else:
 		command = "/usr/bin/sudo /sbin/shutdown -h now"
     
-    process = subprocess.Popen(comando.split(), stdout=subprocess.PIPE)
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print output
 
