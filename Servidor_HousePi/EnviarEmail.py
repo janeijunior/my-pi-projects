78c4fae47b01ff3a439f9377c3a62238db88305a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-01-05 18:50:08 -0200

    Modified EnviarEmail.py

--- Servidor_HousePi/EnviarEmail.py
+++ Servidor_HousePi/EnviarEmail.py
@@ -31,7 +31,8 @@ class EnviarEmail(threading.Thread):
         separador = "-----------------------------------------------------------\n"
         
         
-        conteudo = "O alarme de sua residencia esta disparado. \n" + separado + "Sensor: {idSensor} - {nomeSensor} \n Data e hora do disparo: {dataHora}\n" + separador + "E-mail enviado automaticamento pelo sistema House Pi'''.format(idSensor = int(self.idSensor), nomeSensor = self.nomeSensor, dataHora =  datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        conteudo = "O alarme de sua residencia esta disparado. \n" + separado + "Sensor: {idSensor} - {nomeSensor} \n Data e hora do disparo: {dataHora}\n" + separador + "E-mail enviado automaticamento pelo sistema House Pi".format(
+            idSensor = int(self.idSensor), nomeSensor = self.nomeSensor, dataHora =  datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
         
         print 'Enviando e-mail\n'
         try:
