45e4f25dfa96ac12f73dae02c91d940170f6b3f7
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-01-12 18:17:18 -0200

    Modified Servidor.py

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -537,7 +537,7 @@ def reiniciarDesligarServidor(root, con):
 
 #inicia o servico da camera
 def ligarCamera():
-    os.system("sudo " + MJPG + " start")
+    os.system("sudo " + MJPG + " start 5005 320x240 2") #iniciar, porta, resolucao, fps
     
 #para o servico da camera
 def desligarCamera():
