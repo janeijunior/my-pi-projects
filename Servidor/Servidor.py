e95d5b4b3baed52fc31dad274a02330a59ede358
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-01-15 19:52:29 -0200

    Modified Servidor.py

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -22,7 +22,7 @@ import subprocess
 import select
 
 HOST = ""    # IP do Servidor (em branco = IP do sistema)
-PORT = 5000  # Porta do Servidor
+PORT = 5001  # Porta do Servidor
 SIRENE = 10  # Numero GPIO da sirene
 PLAYLIST = "/home/pi/HousePi/playlist" # Diretorio onde encontra-se a playlist de musicas
 MJPG = "/usr/share/adafruit/webide/repositories/my-pi-projects/Servidor/mjpg-streamer/mjpg-streamer.sh" #caminho stream de video
@@ -492,11 +492,6 @@ def controlarSomAmbiente(root, con):
     elif comando == "Volume":
        executarComandoMPlayer("set_property volume " + valor, "")
     elif comando == "EnviarNomeArquivo":
-        try:
-            nome = executarComandoMPlayer("get_file_name", "ANS_FILENAME")
-        except:
-            nome = ""
-        
         enviarNomeArquivo(con, nome)
 
 #executa um comando no subprocesso do mplayer e devolve o resultado
