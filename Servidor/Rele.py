cfab38aae0bc96d52201775665dd0e56b4f2241b
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-01-20 20:42:21 -0200

    Modified Rele.py

--- Servidor/Rele.py
+++ Servidor/Rele.py
@@ -36,11 +36,13 @@ class Rele(object):
     def ligar(self):
         mcp.output(self.numeroGPIO, LIGAR)
         self.status = STATUS_LIGADO
+        atualizarStatusBanco()
     
     #funcao para desligar o rele
     def desligar(self):
         mcp.output(self.numeroGPIO, DESLIGAR)
         self.status = STATUS_DESLIGADO
+        atualizarStatusBanco()
     
     #funcao para gravar o novo nome do rele
     def gravarNomeBanco(self):
