ec416dd5243f9ea816cf57d2146af3ac58ff3744
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-01-25 11:10:55 -0200

    Modified Servidor.py

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -22,7 +22,7 @@ import subprocess
 import select
 
 HOST = ""    # IP do Servidor (em branco = IP do sistema)
-PORT = 5000  # Porta do Servidor
+PORT = 5001  # Porta do Servidor
 SIRENE = 10  # Numero GPIO da sirene
 PLAYLIST = "/home/pi/HousePi/playlist" # Diretorio onde encontra-se a playlist de musicas
 MJPG = "/usr/share/adafruit/webide/repositories/my-pi-projects/Servidor/mjpg-streamer/mjpg-streamer.sh" #caminho stream de video
@@ -510,6 +510,8 @@ def controlarSomAmbiente(root, con):
             proxima = getPosicaoMusica(valor)
             
             step = proxima - atual
+            
+            print nome, valor, atual, proxima, step
             
             executarComandoMPlayer("pt_step " + step, "")
         except:
