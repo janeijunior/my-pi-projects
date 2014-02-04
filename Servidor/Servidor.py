e0ee4fafd37e0f7f2b1d8d41cf208cd8953dd228
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-02-04 21:24:46 -0200

    Modified Servidor.py

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -21,12 +21,12 @@ import subprocess
 import select
 import MySQLdb
 
-HOST = ""    # IP do Servidor (em branco = IP do sistema)
-PORT = 5000  # Porta do Servidor
-SIRENE = 10  # Numero GPIO da sirene
-PLAYLIST = "/home/pi/HousePi/playlist" # Diretorio onde encontra-se a playlist de musicas
-MUSICAS = "/home/pi/HousePi/Musicas/"   # Diretorio das musicas
-MJPG = "/usr/share/adafruit/webide/repositories/my-pi-projects/Servidor/mjpg-streamer/mjpg-streamer.sh" # Caminho stream de video
+HOST     = ""                                             # IP do Servidor (em branco = IP do sistema)
+PORT     = int(Funcoes.lerConfiguracaoIni("Porta")        # Porta do Servidor
+SIRENE   = int(Funcoes.lerConfiguracaoIni("Sirene")       # Numero GPIO da sirene
+PLAYLIST = Funcoes.lerConfiguracaoIni("CaminhoPlaylist")  # Diretorio onde encontra-se a playlist de musicas
+MUSICAS  = Funcoes.lerConfiguracaoIni("CaminhoMusicas")   # Diretorio das musicas
+MJPG     = Funcoes.lerConfiguracaoIni("CaminhoMJPG")      # Caminho stream de video
 
 orig = (HOST, PORT)
 
