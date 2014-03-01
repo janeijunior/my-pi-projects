33678fee9b8af5e2a5bab98e12d490f3355abfbf
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 18:15:17 -0300

    Modified Funcoes.py

--- Servidor/Funcoes.py
+++ Servidor/Funcoes.py
@@ -23,3 +23,61 @@ def lerConfiguracaoIni(nome):
     cfg.read('Config.ini')
     return cfg.get('Dados', nome)
     
+#funcao para conectar no banco de dados
+def conectarBanco(self):
+    HOST   = Funcoes.lerConfiguracaoIni('HostBanco')
+    USER   = Funcoes.lerConfiguracaoIni('UsuarioBanco')
+    PASSWD = Funcoes.lerConfiguracaoIni('SenhaBanco')
+    BANCO  = Funcoes.lerConfiguracaoIni('NomeBanco')
+
+    try:
+        conBanco = MySQLdb.connect(HOST, USER, PASSWD)
+        conBanco.select_db(BANCO)
+    except MySQLdb.Error, e:
+        print "Nao foi possivel conectar ao banco de dados.", e
+
+    return conBanco
+
+#persiste um SQL no banco de dados
+def executarComando(self, sql):
+    try:
+        conBanco = conectarBanco()
+        cursor = conBanco.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(sql)
+        conBanco.commit()
+        conBanco.close()
+        return True
+    except:
+        conBanco.rollback()
+        conBanco.close()
+        print "Erro ao executar o comando: " + sql
+        return False
+
+#retorna o resultado da consulta SQL em uma linha
+def consultarRegistro(self, sql):
+    try:
+        conBanco = conectarBanco()
+        cursor = conBanco.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        conBanco.close()
+    
+        return row
+    except:
+        print "Erro ao executar o comando: " + sql
+        return None
+
+#retorna o resultado da consulta SQL em varias linhas
+def consultarRegistros(self, sql):
+    try:
+        conBanco = conectarBanco()
+        cursor = conBanco.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        conBanco.close()
+        
+        return rows
+    except:
+        print "Erro ao executar o comando: " + sql
+        return None
+    
