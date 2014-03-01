9450d4055ba4b75dba399d249f8f4c4286f4daa1
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 20:33:43 -0300

    Modified SomAmbiente.py

--- Servidor/SomAmbiente.py
+++ Servidor/SomAmbiente.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import Funcoes
+
 class SomAmbiente(object):
     
     #construtor
     def __init__(self):
-        
+    
+        self.__playlist = Funcoes.lerConfiguracaoIni("CaminhoPlaylist") # Diretorio onde encontra-se a playlist de musicas
+        self.__musicas  = Funcoes.lerConfiguracaoIni("CaminhoMusicas")  # Diretorio das musicas
             
     #funcoes
     
