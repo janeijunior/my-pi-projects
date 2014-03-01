fe892af8fb75ed7b003bede99718ff536e17d3ab
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 18:19:23 -0300

    Modified Funcoes.py

--- Servidor/Funcoes.py
+++ Servidor/Funcoes.py
@@ -5,31 +5,12 @@ import MySQLdb
 import ConfigParser
 from unicodedata import normalize
 
-#remove caracteres invalidos
-def removerAcentos(txt, codif='utf-8'):
-    if  isinstance(txt, unicode):
-        return normalize('NFKD', txt).encode('ASCII','ignore')
-    else:
-        return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
-
-#transforma uma string delimitada em uma lista
-def stringToList(texto):
-    lista = []
-    lista = texto.split(';')
-    return lista
-
-#retorna a configuracao lida do arquivo ini
-def lerConfiguracaoIni(nome):
-    cfg = ConfigParser.ConfigParser()
-    cfg.read('Config.ini')
-    return cfg.get('Dados', nome)
-    
 #funcao para conectar no banco de dados
 def conectarBanco():
-    HOST   = Funcoes.lerConfiguracaoIni('HostBanco')
-    USER   = Funcoes.lerConfiguracaoIni('UsuarioBanco')
-    PASSWD = Funcoes.lerConfiguracaoIni('SenhaBanco')
-    BANCO  = Funcoes.lerConfiguracaoIni('NomeBanco')
+    HOST   = lerConfiguracaoIni('HostBanco')
+    USER   = lerConfiguracaoIni('UsuarioBanco')
+    PASSWD = lerConfiguracaoIni('SenhaBanco')
+    BANCO  = lerConfiguracaoIni('NomeBanco')
 
     try:
         conBanco = MySQLdb.connect(HOST, USER, PASSWD)
@@ -81,4 +62,22 @@ def consultarRegistros(sql):
     except:
         print "Erro ao executar o comando: " + sql
         return None
+
+#remove caracteres invalidos
+def removerAcentos(txt, codif='utf-8'):
+    if  isinstance(txt, unicode):
+        return normalize('NFKD', txt).encode('ASCII','ignore')
+    else:
+        return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore')
+
+#transforma uma string delimitada em uma lista
+def stringToList(texto):
+    lista = []
+    lista = texto.split(';')
+    return lista
+
+def lerConfiguracaoIni(nome):
+    cfg = ConfigParser.ConfigParser()
+    cfg.read('Config.ini')
+    return cfg.get('Dados', nome)
     
