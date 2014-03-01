74c3f05ba1a7ee787f0076a82a75c58a07b5832d
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 20:32:40 -0300

    Modified SomAmbiente.py

---
+++ Servidor/SomAmbiente.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class SomAmbiente(object):
+    
+    #construtor
+    def __init__(self):
+        self.carregarUsuario()
+            
+    #funcoes
+    #funcao para carregar as propriedades da classe
+    def carregarUsuario(self):
+        row = self.consultarRegistro("select Usuario, Senha from Usuario")
+        
+        self.usuario = row["Usuario"]
+        self.senha   = row["Senha"]        
+    
+    #funcao para alterar o usuario e a senha 
+    def alterarUsuarioSenha(self, usuario, senha):        
+        sql = "update Configuracao set Usuario = '{novoUsuario}', Senha = '{novaSenha}'"
+        sql = sql.format(novoUsuario =  usuario, novaSenha = senha)
+        
+        if self.executarComando(sql):
+            con.usuario = usuario
+            con.senha   = senha
+            return True
+        else:
+            return False        
+    
+    #funcao para validar o login do sistema
+    def validarLogin(self, usuario, senha):
+        if (self.usuario == usuario) and (self.senha == senha):
+            return True
+        else:
+            return False
