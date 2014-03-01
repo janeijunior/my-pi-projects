6639213cf361ec330309aa44c8949ebe9b106667
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 20:33:05 -0300

    Modified Servidor.py

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -25,8 +25,6 @@ import Automacao
 HOST     = ""                                            # IP do Servidor (em branco = IP do sistema)
 PORT     = int(Funcoes.lerConfiguracaoIni("Porta"))      # Porta do Servidor
 SIRENE   = int(Funcoes.lerConfiguracaoIni("GPIOSirene")) # Numero GPIO da sirene
-PLAYLIST = Funcoes.lerConfiguracaoIni("CaminhoPlaylist") # Diretorio onde encontra-se a playlist de musicas
-MUSICAS  = Funcoes.lerConfiguracaoIni("CaminhoMusicas")  # Diretorio das musicas
 MJPG     = Funcoes.lerConfiguracaoIni("CaminhoMJPG")     # Caminho stream de video
 
 orig = (HOST, PORT)
