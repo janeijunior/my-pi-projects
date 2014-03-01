9318d35cf99000256e47af894da7a8766fbf43e1
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 20:52:51 -0300

    Modified SomAmbiente.py

--- Servidor/SomAmbiente.py
+++ Servidor/SomAmbiente.py
@@ -17,7 +17,7 @@ class SomAmbiente(object):
     #funcoes
     #retorna a lista de musicas de uma pasta pre determinada
     def getListaMusica(self):
-        playlist = 'find ' + MUSICAS + ' -name "*mp3" -o -name "*m4a" -o -name "*wma" -type f | sort > ' + self.caminhoPlaylist
+        playlist = 'find ' + self.caminhoMusicas + ' -name "*mp3" -o -name "*m4a" -o -name "*wma" -type f | sort > ' + self.caminhoPlaylist
         os.system(playlist)
         arquivo = open(self.caminhoPlaylist)
     
