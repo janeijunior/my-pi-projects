6c612343ff1791db8680c8f3c6d4f26af0bf1b74
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 18:37:10 -0300

    Modified Automacao.py

--- Servidor/Automacao.py
+++ Servidor/Automacao.py
@@ -9,5 +9,19 @@ class Automacao(Base.Base):
     def __init__(self):
         self.usuario = = Usuario.Usuario()
     
-    #funcoes da classe
-    
+    #funcoes da class
+    e
+    #função para validar o usuario e a senha, se nao estiverem certos desconecta!
+    def efetuarLogin(root, con):
+        row = Funcoes.consultarRegistro("select Usuario, Senha from Configuracao")
+        
+        usuario = root.find("Usuario").text.encode('utf-8')
+        senha = root.find("Senha").text.encode('utf-8')
+        
+        if row["Usuario"] == usuario and row["Senha"]  == senha:
+            print "Conectado: ", cliente
+            con.send("Logado\n")
+        else:
+            print "Usuario ou senha invalidos.", cliente
+            con.send("NaoLogado\n")
+            con.close
