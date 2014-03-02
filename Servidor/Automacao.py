874b8b4243d2ba1b178ed94b0f15949a675bdf05
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 21:52:29 -0300

    Modified Automacao.py

--- Servidor/Automacao.py
+++ Servidor/Automacao.py
@@ -12,7 +12,6 @@ class Automacao(Base.Base):
 
     #construtor
     def __init__(self):
-        Base.Base.__init__()
         self.usuario = Usuario.Usuario()
         self.temperaturaHumidade = TemperaturaHumidade.TemperaturaHumidade()
         self.somAmbiente = SomAmbiente.SomAmbiente()
