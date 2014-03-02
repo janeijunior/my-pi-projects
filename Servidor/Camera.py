a54370716263f8b68a1fe000f30baacf07b86ca5
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-02 12:16:43 -0300

    Modified Camera.py

--- Servidor/Camera.py
+++ Servidor/Camera.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import Funcoes
+
 class Camera(object):
     
     #construtor
     def __init__(self):
-    
+        self.MJPG = Funcoes.lerConfiguracaoIni("CaminhoMJPG") # Caminho stream de video
     
     
