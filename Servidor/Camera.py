eee2c364b5d374a1dafaaf0a206741b1103f957b
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-02 14:16:11 -0300

    Modified Camera.py

--- Servidor/Camera.py
+++ Servidor/Camera.py
@@ -13,7 +13,7 @@ class Camera(object):
     
     #inicia o servico da camera
     def ligarCamera():
-        os.system("sudo " + self.MJPG + " start 5005 320x240 2") #iniciar, porta, resolucao, fps
+        os.system("sudo " + self.MJPG) #iniciar, porta, resolucao, fps
         
     #para o servico da camera
     def desligarCamera():
