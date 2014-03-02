db7ef3b8355bbbc79ee696e205c27cc5d9870c4b
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-02 13:33:44 -0300

    Modified Automacao.py

--- Servidor/Automacao.py
+++ Servidor/Automacao.py
@@ -231,7 +231,7 @@ class Automacao(Base.Base):
     def alterarConfiguracaoAlarme(root, con):
         try:
             self.alarme.tempoDisparo = int(root.find("TempoDisparo").text)
-            self.alarme.usarSirene   = int(root.find("UsarSirene").text
+            self.alarme.usarSirene   = int(root.find("UsarSirene").text)
             self.alarme.usarEmail    = int(root.find("UsarEmail").text)
             
             self.alarme.gravarConfiguracao()
