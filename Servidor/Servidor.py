f8519a6eb413d26b57d37c856871afa5b9386447
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-02 15:03:17 -0300

    Modified Servidor.py

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -11,12 +11,10 @@ import Automacao
 import signal
 import sys
 
-HOST     = ""                                       
-PORT     = int(Funcoes.lerConfiguracaoIni("Porta")) 
-
+HOST = ""                                       
+PORT = int(Funcoes.lerConfiguracaoIni("Porta")) 
 orig = (HOST, PORT)
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp.bind(orig)
 tcp.listen(1)
 
