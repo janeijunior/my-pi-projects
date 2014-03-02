907514a0986a7ab3a75862c1c86be556e8eac4ee
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-03-01 21:42:01 -0300

    Modified SomAmbiente.py

--- Servidor/SomAmbiente.py
+++ Servidor/SomAmbiente.py
@@ -10,6 +10,7 @@ class SomAmbiente(object):
     
     #construtor
     def __init__(self): 
+        Base.__init__()
         self.__caminhoPlaylist = Funcoes.lerConfiguracaoIni("CaminhoPlaylist") # Diretorio onde encontra-se a playlist de musicas
         self.__caminhoMusicas  = Funcoes.lerConfiguracaoIni("CaminhoMusicas")  # Diretorio das musicas
         self.__mplayer = None
