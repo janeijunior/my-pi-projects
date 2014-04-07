3a941e117356c80acab482923f14d72c745c8ba6
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-04-06 23:26:45 -0300

    Modified Automacao.py

--- Servidor/Automacao.py
+++ Servidor/Automacao.py
@@ -106,8 +106,6 @@ class Automacao(Base.Base):
             self.camera.desligar()
             time.sleep(1)
             
-            print "Chama classe"
-            
             resultado = self.temperaturaHumidade.getDados()    
             root  = Element("TemperaturaHumidade")
             dados = Element("Dados", Temperatura=resultado[0], Humidade=resultado[1])
