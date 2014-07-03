dc663c14226ed5c012db2b08588d5b937b2091a5
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-07-02 22:38:02 -0300

    Alterado para AnsyTask o procedimento de stream da imagem da camera. Isso evita o aspecto travado da aplicação.

--- Android/HousePi/src/br/com/housepi/activity/VisualizacaoCamera.java
+++ Android/HousePi/src/br/com/housepi/activity/VisualizacaoCamera.java
@@ -1,16 +1,17 @@
 package br.com.housepi.activity;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import java.net.URI;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.output.XMLOutputter;
-
 import br.com.housepi.activity.Login;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -24,7 +25,6 @@ import br.com.housepi.classes.Conexao;
 public class VisualizacaoCamera extends Fragment {
 
 	private MjpegView mv;
-	private HttpURLConnection con;
 
 	public static Fragment newInstance(Context context) {
 		VisualizacaoCamera f = new VisualizacaoCamera();
@@ -35,33 +35,20 @@ public class VisualizacaoCamera extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.visualizacao_camera, container, false);
 
-		controlarCamera("Ligar");
-		
-		mv = (MjpegView) rootView.findViewById(R.id.mjpeg_view);
-		
 		Integer portaCamera = Integer.parseInt(Login.PORTA_SERVIDOR) + 1;
+		String URL = "http://" + Login.IP_SERVIDOR + ":" + portaCamera.toString() + "/?action=stream";
 		
-		String url = "http://" + Login.IP_SERVIDOR + ":" + portaCamera.toString() + "/?action=stream"; 
-
 		mv = (MjpegView) rootView.findViewById(R.id.mjpeg_view);
-		connection(mv, url);
+		
+		new DoRead().execute(URL);
 
 		return rootView;
 	}
 
-	private void connection(MjpegView mv, String url) {
-		try {
-			URL addr = new URL(url);
-			con = (HttpURLConnection) addr.openConnection();
-			con.connect();
-			InputStream stream;
-			stream = con.getInputStream();
-			mv.setSource(new MjpegInputStream(stream));
-			mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
-			mv.showFps(false);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void onPause() {
+		mv.stopPlayback();
+		controlarCamera("Desligar");
+		super.onPause();		
 	}
 	
 	private void controlarCamera(String comando){
@@ -78,11 +65,32 @@ public class VisualizacaoCamera extends Fragment {
 		
 		Conexao.getConexaoAtual().receberRetorno().equals("Ok");
 	}
-	
-	@Override
-	public void onPause() {
-		con.disconnect();
-		controlarCamera("Desligar");
-		super.onPause();
+
+	public class DoRead extends AsyncTask<String, Void, MjpegInputStream> {
+		protected MjpegInputStream doInBackground(String... url) {
+			HttpResponse res = null;
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			try {
+				controlarCamera("Ligar");
+				
+				res = httpclient.execute(new HttpGet(URI.create(url[0])));
+				if (res.getStatusLine().getStatusCode() == 401) {
+					return null;
+				}
+				return new MjpegInputStream(res.getEntity().getContent());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		protected void onPostExecute(MjpegInputStream result) {
+			mv.setSource(result);
+			mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+			mv.showFps(false);
+		}
 	}
 }
