2a80bade7e2281875d12e916d7ebd6b9d292269c
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-17 19:06:22 -0300

    Modified pyomxplayer.py

--- Servidor/pyomxplayer/pyomxplayer.py
+++ Servidor/pyomxplayer/pyomxplayer.py
@@ -196,7 +196,7 @@ def check_input (videodir,track):
         
 # MAIN
 # change videodir to where you store your videos. Must be a path from root
-videodir ="/home/pi/HousePi/Videos/pyomxplayer/"
+videodir ="/home/pi/HousePi/Videos/"
 debug = False
 
 
