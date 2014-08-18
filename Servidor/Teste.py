241ed028a8d3fd6d8bd0c7446635f3ebaae42c38
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-17 21:14:33 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -1,6 +1,6 @@
 from pyomxplayer import OMXPlayer
 from pprint import pprint
-omx = OMXPlayer('/home/pi/HousePi/Videos/Lilly.mp4')
+omx = OMXPlayer('/home/pi/HousePi/Videos/Ariana.mp4')
 pprint(omx.__dict__)
 omx.toggle_pause()
 #omx.position
