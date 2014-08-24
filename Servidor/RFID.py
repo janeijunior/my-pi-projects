d5070b85c0e522c776053eaeebf033e27d1be448
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-24 16:07:52 -0300

    Modified RFID.py

--- Servidor/RFID.py
+++ Servidor/RFID.py
@@ -16,7 +16,7 @@ class RFID(threading.Thread):
         
         #atributos
         self.alarme = alarme
-        self.serial = serial.Serial('/dev/tty0', 9600, timeout=2)
+        self.serial = serial.Serial('/dev/tty0', 1200, timeout=1)
                 
     def stop(self):
         self.serial.close()
