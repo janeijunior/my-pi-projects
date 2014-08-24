36390f4dff98bba8b5bec8b519e1fb43c590f067
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-24 11:53:13 -0300

    Removed Teste.py

--- Servidor/Teste.py
+++
@@ -1,7 +0,0 @@
-from pyomxplayer import OMXPlayer
-from pprint import pprint
-omx = OMXPlayer('/home/pi/HousePi/Videos/Ariana.mp4')
-pprint(omx.__dict__)
-omx.toggle_pause()
-#omx.position
-omx.stop()
