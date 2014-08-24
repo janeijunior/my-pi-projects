cb27291a07e88be34fd9652dd50aa8a7bdc470da
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-24 14:15:19 -0300

    Modified RFID.py

--- Servidor/RFID.py
+++ Servidor/RFID.py
@@ -1,29 +1,32 @@
-#import serial
-#serial = serial.Serial("/dev/ttyUSB0", baudrate=9600)
-#
-#code = ''
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
 
-#while True:
-#        data = serial.read()
-#        if data == '\r':
-#                print(code)
-#                code = ''
-#        else:
-#                code = code + data
-
+import thread
+import threading
 import time
 import sys
 
-card = '0019171125'
+card1 = '0007181175'
+card2 = '0008056554'
 
-def main():
-    with open('/dev/tty1', 'r') as tty:
-        while True:
-            RFID_input = tty.readline()
-            if RFID_input == card:
-                print "Access Granted" 
-                print "Read code from RFID reader:{0}".format(RFID_input)
-            else:
-                print "Access Denied"
+class RFID(threading.Thread):
+    def __init__(self, alarme):
+        threading.Thread.__init__(self)
+        self.name = 'ThreadRFID'
+        self.__stop_thread_event = threading.Event()
+        
+        #atributos
+        self.alarme = alarme
                 
-main()
+    def stop(self):
+        self.__stop_thread_event.set()
+        
+    def run(self):
+        with open('/dev/tty4', 'r') as tty:
+            while True:
+                RFID_input = tty.readline()
+                if (RFID_input == card1) :
+                    print "Access Granted" 
+                    print "Read code from RFID reader:{0}".format(RFID_input)
+                else:
+                    print "Access Denied"
