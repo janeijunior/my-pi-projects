535770f278cce07adcd075a8a065cc2f62d10301
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-24 16:55:38 -0300

    Modified Teste.py

---
+++ Servidor/Teste.py
@@ -0,0 +1,13 @@
+import serial
+
+serial = serial.Serial("/dev/tty", baudrate=9600)
+
+code = ''
+
+while True:
+        data = serial.read()
+        if data == '\r':
+                print(code)
+                code = ''
+        else:
+                code = code + data
