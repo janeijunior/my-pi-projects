6ce52d0701d611737f6c850a23fd03f0a0ccd908
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-24 15:17:13 -0300

    Modified RFID.py

--- Servidor/RFID.py
+++ Servidor/RFID.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import thread
-import threading
-import time
-import sys
-import serial
+#import thread
+#import threading
+#import time
+#import sys
+#import serial
+#
+#card1 = '0007181175'
+#card2 = '0008056554'
+#
+#class RFID(threading.Thread):
+#    def __init__(self, alarme):
+#        threading.Thread.__init__(self)
+#        self.name = 'ThreadRFID'
+#        self.__stop_thread_event = threading.Event()
+#        
+#        #atributos
+#        self.alarme = alarme
+#                
+#    def stop(self):
+#        self.__stop_thread_event.set()
+#        
+#    def run(self):
+#        while True:
+#            dados = raw_input()
+#            
+#            if dados <> '':
+#                print dados
+#                
+#                if (dados == card1) or (dados == card2):
+#                    print "Acesso Permitido."
+#                    
+#                    if self.alarme.alarmeLigado:
+#                        self.alarme.ligarAlarme()
+#                    else:
+#                        self.alarme.desligarAlarme()
+#                else:
+#                    print "Acesso Negado."
+#                
+#                time.sleep(1.5)
 
-card1 = '0007181175'
-card2 = '0008056554'
-
-class RFID(threading.Thread):
-    def __init__(self, alarme):
-        threading.Thread.__init__(self)
-        self.name = 'ThreadRFID'
-        self.__stop_thread_event = threading.Event()
-        
-        #atributos
-        self.alarme = alarme
-                
-    def stop(self):
-        self.__stop_thread_event.set()
-        
-    def run(self):
-        while True:
-            dados = raw_input()
-            
-            if dados <> '':
-                print dados
-                
-                if (dados == card1) or (dados == card2):
-                    print "Acesso Permitido."
-                    
-                    if self.alarme.alarmeLigado:
-                        self.alarme.ligarAlarme()
-                    else:
-                        self.alarme.desligarAlarme()
-                else:
-                    print "Acesso Negado."
-                
-                time.sleep(1.5)
