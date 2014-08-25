3ca9e3f313b418530422af01344a6a3eb8694570
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 15:45:11 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -9,6 +9,7 @@ def main():
     with open('/dev/tty0', 'r') as tty:
         while True:
             RFID_input = tty.readline()
+            
             if RFID_input == card:
                 print "Access Granted: {0}".format(RFID_input)
             else:
