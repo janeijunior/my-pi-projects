46c06aa5ef324f93c8bb9cb2267b8bd297176f1e
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 16:10:42 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
-import time
-import sys
+#import time
+#import sys
 
-card = ['0007181175']
+#card = ['0007181175']
 
-def main():
-    with open('/dev/tty1', 'r') as tty:
-        while True:
-            RFID_input = tty.readline().rstrip()
+#def main():
+#    with open('/dev/tty1', 'r') as tty:
+#        while True:
+#            RFID_input = tty.readline().rstrip()
             
-            if RFID_input in card:
-                print "Access Granted: {0}".format(RFID_input)
-            else:
-                print "Access Denied: {0}".format(RFID_input)
+#            if RFID_input in card:
+#                print "Access Granted: {0}".format(RFID_input)
+#            else:
+#                print "Access Denied: {0}".format(RFID_input)
             
-main()
+#main()
+
