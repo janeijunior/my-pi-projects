f678804634020514e7c525630e61540e477263ef
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 16:22:29 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -9,14 +9,15 @@ def main():
     while True:
         try:
             with open('/dev/tty1', 'r') as tty:
-            RFID_input = tty.readline().rstrip()
-            
-            if RFID_input in card:
-                print "Access Granted: {0}".format(RFID_input)
-            else:
-                print "Access Denied: {0}".format(RFID_input)
-            
-            tty.close()
-            
+                RFID_input = tty.readline().rstrip()
+                
+                if RFID_input in card:
+                    print "Access Granted: {0}".format(RFID_input)
+                else:
+                    print "Access Denied: {0}".format(RFID_input)
+                
+                tty.close()
+        except:
+            print "Erro ao abrir o arquivo."            
 main()
 
