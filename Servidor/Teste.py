0a4d25d455a67db012b197e81b594d65d735895b
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 18:26:09 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -3,27 +3,11 @@
 import sys
 import thread
 import threading
-
-card = ['0007181175', '0008056554']
+import RFID
 
 def main():
-    thread = threading.Thread(None, lerDados, None, ())
-    thread.start()
-            
-def lerDados():
-    while True:
-        try:
-            with open('/dev/tty1', 'r') as tty:
-                RFID_input = tty.readline().rstrip()
-                
-                if RFID_input in card:
-                    print "Acesso Permitido: {0}".format(RFID_input)
-                else:
-                    print "Acesso Negado: {0}".format(RFID_input)
-                
-                tty.close()
-        except:
-            print "Erro ao abrir o arquivo."
-            
+    RFID = RFID.RFID(None)
+    RFID.start() 
+
 main()
 
