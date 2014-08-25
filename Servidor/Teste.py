29cb4f47cc6bb93f63c79b9af46c627e49b47598
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 15:46:55 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -8,7 +8,9 @@ card = ['0007181175']
 def main():
     while True:
         sys.stdin = open('/dev/tty0', 'r')
-        RFID_input = input()
+        
+        RFID_input = str(input())
+        
         if RFID_input == card:
             print "Access Granted"
             print "Read code from RFID reader:{0}".format(RFID_input)
