19fe9965d93c112b5ef367afd60cd9d26c133aec
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 19:17:59 -0300

    Modified ControleAgendamento.py

--- Servidor/ControleAgendamento.py
+++ Servidor/ControleAgendamento.py
@@ -91,4 +91,4 @@ class ControleAgendamento(threading.Thread):
                                 
                             agendamento.desativarRegistroBanco()
                 
-            #time.sleep(0.15) 
+            time.sleep(0.15) 
