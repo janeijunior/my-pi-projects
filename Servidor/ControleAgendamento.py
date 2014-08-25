0aeaf306fcdd6b5f70182416d2c5c976bc12e637
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 19:11:04 -0300

    Modified ControleAgendamento.py

--- Servidor/ControleAgendamento.py
+++ Servidor/ControleAgendamento.py
@@ -24,6 +24,8 @@ class ControleAgendamento(threading.Thread):
     def run(self):
         #executa enquanto nao setar o evento
         while not self.__stop_thread_event.isSet(): 
+            temp = temp + 1;            
+            
             #data e hora atual
             atual     = datetime.now().strftime("%Y%m%d%H%M%S")
             diaAtual  = datetime.now().strftime("%Y%m%d") 
