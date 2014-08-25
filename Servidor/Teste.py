5c7e78ad9a72f8fb11e44af50b84eb07ab186c1d
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 17:56:50 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -10,20 +10,20 @@ def main():
     thread = threading.Thread(None, self.lerDados, None, ())
     thread.start()
             
-def lerDados(self):
-    while True:
-        try:
-            with open('/dev/tty1', 'r') as tty:
-                RFID_input = tty.readline().rstrip()
-                
-                if RFID_input in card:
-                    print "Acesso Permitido: {0}".format(RFID_input)
-                else:
-                    print "Acesso Negado: {0}".format(RFID_input)
-                
-                tty.close()
-        except:
-            print "Erro ao abrir o arquivo."
+    def lerDados(self):
+        while True:
+            try:
+                with open('/dev/tty1', 'r') as tty:
+                    RFID_input = tty.readline().rstrip()
+                    
+                    if RFID_input in card:
+                        print "Acesso Permitido: {0}".format(RFID_input)
+                    else:
+                        print "Acesso Negado: {0}".format(RFID_input)
+                    
+                    tty.close()
+            except:
+                print "Erro ao abrir o arquivo."
             
 main()
 
