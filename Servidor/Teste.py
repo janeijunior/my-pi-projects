61ef82e54b2a1684ae19b4e69a0a0d0d2efe3b2f
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 15:33:53 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -1,30 +1,16 @@
-# Notes
-# Find the RFID reader within Raspberry Pi 
-# $ ls /dev/tty*
-
-import time
+# python
 import serial
 
-delay  = 0
-port   = 2400
-tag_death = "0007181175"
-serial = serial.Serial('/dev/tty0', port, timeout=1)
+s = serial.Serial(
+    '/dev/tty0',
+    port=0,
+    parity=serial.PARITY_NONE,
+    bytesize=serial.EIGHTBITS,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=3,
+    xonxoff=0,
+    rtscts=0,
+    baudrate=2400
+   )
 
-def check():
-  #The tag is a 12-byte string starting with a carriage
-  #return and ending with a newline return EM4001 tags will
-  #never contain anything but ASCII digits 0-9A-F
-  unique_id = serial.readline()
-  
-  if unique_id == tag_death:
-    print("You found the death key")
-    exit()
-  else:
-      print("Sandy's Super Access Code:", unique_id)
-
-try:
-  while True:
-    time.sleep(delay)
-    check()
-except KeyboardInterrupt:
-  exit()
+print s.readline()
