2cd26d1fcd1b336a72c497e2c9e1d119e5ffc72f
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-25 22:30:01 -0300

    Modified Automacao.py

--- Servidor/Automacao.py
+++ Servidor/Automacao.py
@@ -212,6 +212,8 @@ class Automacao(Base.Base):
 
     #liga ou desliga o alarme se validar o RFID
     def controlarRFID(self, root):
+        print 'rfid'
+        
         for child in root:
             cartao = str(child.get("Cartao")) 
         
