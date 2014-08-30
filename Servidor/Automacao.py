ece72b25a2b11517c14b46b3de4657a01c7a064d
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-08-30 20:48:36 -0300

    Modified Automacao.py

--- Servidor/Automacao.py
+++ Servidor/Automacao.py
@@ -117,7 +117,7 @@ class Automacao(Base.Base):
     #le o sensor de temperatura e humidade e envia os resultados
     def enviarTemperaturaHumidade(self, con):    
         sensor = Adafruit_DHT.DHT22
-        humidade, temperatura = Adafruit_DHT.read_retry(sensor, int(Funcoes.lerConfiguracaoIni("GPIODHT")))
+        humidade, temperatura = Adafruit_DHT.read_retry(sensor, self.GPIODHT)
         
         if humidade is not None and temperatura is not None:            
             print 'Temperatura={0:0.1f}*C  Humidade={1:0.1f}%'.format(temperatura, humidade)
