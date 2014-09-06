ec0e98d3259364d01308ed1737f63e8b641b0d31
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-09-06 11:22:20 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -4,7 +4,7 @@ import pygame, sys, os
 
 
 if __name__ == "__main__":
-	pygame.init()
+    pygame.init()
     pygame.mixer.init
     pygame.mixer.music.load('/home/pi/HousePi/Musicas/01.\ John\ Legend\ -\ John\ Legend\ -\ All\ Of\ Me.mp3')
     pygame.mixer.music.play(0)
