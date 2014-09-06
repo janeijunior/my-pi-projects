2b33b7a6a242241bb9c82d36faa028bc87ba89a4
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-09-06 11:13:33 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -1,294 +1,254 @@
-#!/usr/bin/python
+#!/usr/bin/env pythonw
+#
+#   sound.py
+#   pymage
+#
+#   Copyright (C) 2006-2007 Ross Light
+#
+#   This file is part of pymage.
+#
+#   pymage is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#   
+#   pymage is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#   
+#   You should have received a copy of the GNU Lesser General Public
+#   License along with pymage; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+#   USA
+#
 
-# The Sound Engine
-# Provides helper functions and a playlist class
-# Zach Bernal | Summer 2009
+"""pymage sound subsystem"""
 
-import pygame, sys, os
-import math, copy, random
-if not pygame.mixer: print 'Warning, pygame.mixer did not load. Sound disabled.'
+import warnings
 
-__all__ = ["init", "isenabled", "NoneSound", "load_sound", "load_music", "Playlist"]
+import pygame
+from pygame.locals import *
 
-__audio_enabled = False
+from pymage import resman
 
-def init():
-    """ A wrapper for pygame.mixer.init(). """
-    global __audio_enabled
-    try:
-	    if not __audio_enabled:
-			pygame.mixer.init(22050, -16, 2, 1024) # in pygame 1.9, third argument is stereo if 2.
-			#pygame.mixer.init()
-			__audio_enabled = True
-			print "Audio Enabled."
-    except pygame.error, message :
-		__audio_enabled = False
-		print "Audio disabled:", message
-    return __audio_enabled
+__author__ = 'Ross Light'
+__date__ = 'July 19, 2006'
+__all__ = ['MusicManager',
+           'music',
+           'SoundManager',
+           'sound',]
+__docformat__ = 'reStructuredText'
 
-def isenabled():
-	return __audio_enabled
+class MusicManager(resman.Submanager):
+    """
+    Game music manager.
+    
+    For sound effects, use `SoundManager`.
+    """
+    endEvent = 25
+    
+    def __init__(self,
+                 should_play=True,
+                 loop=True,
+                 *args, **kw):
+        super(MusicManager, self).__init__(*args, **kw)
+        self.savedPlaylists = {}
+        self.playlist = []
+        self.loop = loop
+        self.shouldPlay = should_play
+        self.index = 0
+        self.playing = False
+        pygame.mixer.music.set_endevent(self.endEvent)
+    
+    # Playlist management
+    
+    def prepare(self, tag, playlist):
+        """
+        Prepares a playlist for playback later.
+        
+        .. Warning::
+           `prepare` is retained for compatibility reasons.  You should be using
+           `addPlaylist` to add new resources.
+        """
+        warnings.warn("prepare is deprecated; use addPlaylist.",
+                      DeprecationWarning,
+                      stacklevel=2)
+        self.addPlaylist(tag, playlist)
+    
+    def addPlaylist(self, key, songs):
+        """Adds a new playlist."""
+        self.savedPlaylists[key] = list(songs)
+    
+    def getPlaylist(self, key):
+        """Accesses the playlist."""
+        return self.savedPlaylists[key]
+    
+    def removePlaylist(self, key):
+        """Removes a saved playlist."""
+        del self.savedPlaylists[key]
+    
+    # Playback control
+    
+    def startPlaylist(self, key):
+        """
+        Starts a playlist denoted by ``key``.
+        
+        You may pass an iterable object to `startPlaylist`, and the object will
+        be used as a playlist.
+        
+        After calling `startPlaylist`, the playlist is *cued*.  You then need to
+        call `play` to start playing music.
+        """
+        try:
+            self.playlist = self.getPlaylist(key)
+        except KeyError:
+            try:
+                self.playlist = list(key)
+            except TypeError:
+                raise TypeError("Playlist must be added or an iterable object")
+        if self.playlist:
+            self.index = 0
+            self.loadSong()
+    
+    def play(self):
+        """Starts the current song."""
+        if self.shouldPlay and not self.playing:
+            if pygame.mixer.music.get_busy():
+                pygame.mixer.music.unpause()
+            else:
+                pygame.mixer.music.play()
+            self.playing = True
+    
+    def pause(self):
+        """
+        Pauses the current song.
+        
+        You can resume using `play`.
+        """
+        pygame.mixer.music.pause()
+        self.playing = False
+    
+    def stop(self):
+        """Stops the current song and goes to the beginning."""
+        pygame.mixer.music.stop()
+        self.playing = False
+    
+    def load(self, key=None):
+        """
+        Manually loads a song.
+        
+        If no argument is given, the current song in the playlist is loaded.
+        This method will not load anything unless the manager is configured to
+        play.
+        """
+        if key is None:
+            if self.playlist:
+                key = self.playlist[self.index]
+            else:
+                raise ValueError("No song in the playlist")
+        if self.shouldPlay:
+            super(MusicManager, self).load(key)
+    
+    loadSong = load
+    
+    def previousSong(self):
+        """Returns to the previous song in the playlist."""
+        if self.playlist:
+            self.index -= 1
+            if self.index < 0:
+                self.index = len(self.playlist) - 1
+            self.stop()
+            self.loadSong()
+        
+    def nextSong(self):
+        """Advances to the next song in the playlist."""
+        if self.playlist:
+            playNextSong = self.playing
+            self.index += 1
+            if self.index >= len(self.playlist):
+                self.index = 0
+                if not self.loop:
+                    playNextSong = False
+            self.stop()
+            self.loadSong()
+            if playNextSong:
+                self.play()
+    
+    # Volume control
+    
+    def getVolume(self):
+        """
+        Gets the current volume.
+        
+        You can also use the ``volume`` property.
+        """
+        return self.volume
+    
+    def setVolume(self, volume):
+        """
+        Changes the current volume.
+        
+        You can also use the ``volume`` property.
+        """
+        self.volume = volume
+    
+    def _getVolume(self):
+        """Retrieves the mixer's current volume."""
+        return pygame.mixer.music.get_volume()
+    
+    def _setVolume(self, volume):
+        """Changes the mixer's current volume."""
+        pygame.mixer.music.set_volume(volume)
+    
+    volume = property(_getVolume, _setVolume, doc="The mixer's volume")
 
-class NoneSound:
-	def play(self): pass
-	def stop(self): pass
-	def fadeout(self, *args): pass
-	def set_volume(self, *args): pass
-	def get_volume(self, *args): pass
-	def get_num_channels(self, *args): pass
-	def get_length(self, *args): pass
-	def get_buffer(self, *args): pass
+music = MusicManager()
 
+class SoundManager(resman.Submanager):
+    """
+    Sound effects manager.
+    
+    For music, use `MusicManager`.
+    """
+    resourceType = resman.SoundResource
+    
+    def __init__(self, should_play=True, volume=0.5, *args, **kw):
+        super(SoundManager, self).__init__(*args, **kw)
+        self.shouldPlay = should_play
+        self.volume = volume
+    
+    def getSound(self, *args, **kw):
+        """
+        Retrieves a sound, using a cache if possible.
 
-#sound loader
-def load_sound(name, dirname = 'sounds', soundcache = {}):
+        .. Warning::
+           `getSound` is deprecated, for favor of the Submanager API.  Use
+           `load` instead.
+        """
+        warnings.warn("getSound is deprecated; use load.",
+                      DeprecationWarning,
+                      stacklevel=2)
+        return self.load(*args, **kw)
+    
+    def play(self, tag, volume=None, cache=True):
+        """
+        Plays a sound and returns the sound object.
+        
+        This method will use a cached representation if possible.  Also, you can
+        specify a non-default volume, if desired.  The cache flag specifies
+        whether the sound will be cached, not whether it uses the cache.
+        """
+        if volume is None:
+            volume = self.volume
+        if self.shouldPlay:
+            if cache:
+                self.cache(tag)
+            snd = self.load(tag)
+            snd.set_volume(volume)
+            snd.play()
+            return snd
+        else:
+            return None
 
-	if not pygame.mixer or not __audio_enabled:
-		return NoneSound()
-	fullname = os.path.join(dirname, name)
-
-	if fullname in soundcache:
-		return soundcache[fullname]
-
-	try:
-		sound = pygame.mixer.Sound(fullname)
-	except pygame.error, message:
-		print "Cannot load sound:", fullname
-		raise SystemExit, message
-
-	# Cache sound
-	soundcache[fullname] = sound
-	return sound
-
-#music loader
-def load_music(name, dirname = 'bgm'):
-	if not pygame.mixer or not __audio_enabled:
-		return False
-	fullname = os.path.join(dirname, name)
-	try:
-		music = pygame.mixer.music.load(fullname)
-	except pygame.error, message:
-		print "Cannot load music:", fullname
-		raise SystemExit, message
-	return True
-
-
-
-
-class Playlist():
-	""" A Macintosh-style playlist abstraction """
-	NORANDOM, RANDOM = range(2)
-	NOLOOP, LOOP, LOOPONE  = range(3)
-	STOP, PLAY, PAUSE = range(3)
-	PASSIVE, SPECIFIC, NEXT, PREV = range(4)
-
-	def __init__(self):
-
-		# initialize mixer
-		if not init():
-			return None
-
-		# State machine states
-		self.randomstate = Playlist.NORANDOM # whether to use random or incrementing tracklist
-		self.loopstate = Playlist.NOLOOP # whether to loop at end of list or on single track or to stop at end.
-		self.pausestate = Playlist.STOP # whether to play, stop, or pause playback.
-		self.prev_pausestate = Playlist.STOP # previous state, for edge cases.
-		self.controlstate = Playlist.PASSIVE # overrides loop and random states to switch between tracks
-
-		# list of filenames to play
-		self.playlist = []
-		self.randomindexes = range(0, len(self.playlist))
-
-		# current track starts at -1, gets incremented to first in list
-		self.index = -1
-		
-		# need a random number generator for random play...
-		# using the python native instance
-		#random.seed(time.time())
-
-	def update(self):
-		""" Update internal state of playlist and pygame music player. Return true if playing or paused, return false if stopped. """
-		# Don't allow empty playlists to play.
-		if len(self.playlist) == 0:
-			pass
-			###print "nothing to play!"
-		# if playing from stop or play... (will contine to play tracks...)
-		elif self.pausestate == Playlist.PLAY and self.prev_pausestate != Playlist.PAUSE:
-			# Start the next track, if we're done playing the current one..
-			if not pygame.mixer.music.get_busy():
-
-				# what do we do when we run out of random indexes?
-				# loop: reset them, otherwise we've finished the playlist.
-				if len(self.randomindexes) == 0:
-					if self.loopstate == Playlist.LOOP:
-						self._resetrandom()
-					else:
-						print "Done playing random indexes."
-						self.restart()
-						return False
-
-				#print "random indexes:", self.randomindexes
-
-				# what is the next track?
-				# control states and the "loop one" setting override everything else
-				if self.controlstate == Playlist.SPECIFIC:
-					# keep self.index the same, it was set in self.play()
-					if self.index in self.randomindexes: 
-						self.randomindexes.remove(self.index)
-					self.controlstate = Playlist.PASSIVE # the control expires after execution.
-				elif self.controlstate == Playlist.NEXT:
-					self.index += 1	
-					self.controlstate = Playlist.PASSIVE # the control expires after execution.
-				elif self.controlstate == Playlist.PREV:
-					self.index -= 1	
-					if self.index < 0:
-						self.index = len(self.playlist)-1
-					self.controlstate = Playlist.PASSIVE # the control expires after execution.
-
-				elif self.loopstate == Playlist.LOOPONE:
-					# keep self.index the same
-					if self.index in self.randomindexes: 
-						self.randomindexes.remove(self.index)
-				# either increment in random sequence or increment by one
-				elif self.randomstate == Playlist.RANDOM:
-					# get a random thing from the remaining items not yet chosen
-					#self.index = random.randint(0, len(self.playlist)-1)
-					self.index = random.choice(self.randomindexes)
-					self.randomindexes.remove(self.index)
-				else: # increment
-					self.index += 1
-
-				# what do we do when we hit the end?
-				# loop: go to 0, otherwise we've finished the playlist.
-				if self.index >= len(self.playlist):
-					if self.loopstate == Playlist.LOOP:
-						self.index = 0
-					else:
-						print "Done playing the list."
-						self.restart()
-						return False
-
-				# load & play next track
-				load_music(self.playlist[self.index], "")
-				print str(self.index) + ":", self.playlist[self.index], "r:", self.randomstate, "l:", self.loopstate
-				pygame.mixer.music.play()
-			return True
-
-		# if playing from pause...
-		elif self.pausestate == Playlist.PLAY and self.prev_pausestate == Playlist.PAUSE:
-			pygame.mixer.music.unpause()
-			self.prev_pausestate == Playlist.PLAY # forget about the pause so that more tracks will be played
-			return True
-			
-		# if pausing (do this once coming from play, then stop pausing after that)
-		elif self.pausestate == Playlist.PAUSE and self.prev_pausestate == Playlist.PLAY:
-			pygame.mixer.music.pause()
-			self.prev_pausestate = Playlist.PAUSE # forget to keep pausing
-			return True
-		
-		# if stopping from play or pause...
-		elif self.pausestate == Playlist.STOP and self.prev_pausestate != Playlist.STOP:
-			pygame.mixer.music.stop()
-			self.prev_pausestate = Playlist.STOP # forget to keep stopping
-			return False
-
-
-	def _play(self, index = -1):
-		if index > -1:
-			# force this index to be played.
-			self.index = index
-			self.controlstate = Playlist.SPECIFIC
-		self.prev_pausestate = self.pausestate
-		self.pausestate = Playlist.PLAY
-	def _pause(self):
-		self.prev_pausestate = self.pausestate
-		self.pausestate = Playlist.PAUSE
-	def _stop(self):
-		self.prev_pausestate = self.pausestate
-		self.pausestate = Playlist.STOP
-	def _next(self):
-		self.controlstate = Playlist.NEXT
-	def _prev(self):
-		self.controlstate = Playlist.PREV
-
-	def _restart(self):
-		self._stop()
-		self.index = -1
-		self._resetrandom()
-
-	def _resetrandom(self):
-		#random.seed(time.time())
-		self.randomindexes = range(0, len(self.playlist))
-		print "reset random indexes"
-
-	def play(self, index = -1):
-		self._play(index)
-		self.update()
-
-	def next(self):
-		self._stop()
-		self.update()
-		self._next()
-		self._play()
-
-	def prev(self):
-		self._stop()
-		self.update()
-		self._prev()
-		self._play()
-
-	def pause(self):
-		if self.pausestate == Playlist.PAUSE:
-			self.play()
-		else:
-			self._pause()
-			self.update()
-
-	def stop(self):
-		self._stop()
-		self.update()
-
-	def add(self, filename, dirname = "bgm"):
-		fullname = os.path.join( dirname, filename)
-		self.playlist.append(fullname)
-		self._resetrandom()
-
-	def addfolder(self, foldername = "bgm"):
-		for filename in os.listdir(foldername):
-			if filename.endswith(".mp3") or filename.endswith(".wav"):
-				self.add(filename, foldername)
-
-	def setrandom(self):
-		self.randomstate = Playlist.RANDOM
-	def unsetrandom(self):
-		self.randomstate = Playlist.NORANDOM
-	def setloop(self):
-		self.loopstate = Playlist.LOOP
-	def setloopone(self):
-		self.loopstate = Playlist.LOOPONE
-	def unsetloop(self):
-		self.loopstate = Playlist.NOLOOP
-
-	def set_volume(self, vol):
-		pygame.mixer.music.set_volume(vol)
-
-	def get_volume(self):
-		return pygame.mixer.music.get_volume()
-	def __len__(self):
-		return len(self.playlist);
-
-
-if __name__ == "__main__":
-	player = Playlist()
-
-	player.addfolder("/home/pi/HousePi/Musicas")
-
-	def playloop(player):
-		player.play()
-		while player.update():
-			pass
-
-	# hey look, interactive shell!
-	import code
-	shell = code.InteractiveConsole(globals())
-	shell.interact()
+sound = SoundManager()
