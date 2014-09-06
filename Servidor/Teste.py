c0d92eab79bd6412986c3390fff78b91f8678cb6
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-09-06 11:10:04 -0300

    Modified Teste.py

--- Servidor/Teste.py
+++ Servidor/Teste.py
@@ -6,7 +6,6 @@
 
 import pygame, sys, os
 import math, copy, random
-
 if not pygame.mixer: print 'Warning, pygame.mixer did not load. Sound disabled.'
 
 __all__ = ["init", "isenabled", "NoneSound", "load_sound", "load_music", "Playlist"]
@@ -14,38 +13,42 @@ __all__ = ["init", "isenabled", "NoneSound", "load_sound", "load_music", "Playli
 __audio_enabled = False
 
 def init():
-    global __audio_enabled
-    try:
-        if not __audio_enabled:
-            pygame.mixer.init(22050, -16, 2, 1024) # in pygame 1.9, third argument is stereo if 2.
-            __audio_enabled = True
-            print "Audio Enabled."
-    except pygame.error, message:
-        __audio_enabled = False
-        print "Audio disabled:", message
-    return __audio_enabled
+    """ A wrapper for pygame.mixer.init(). """
+	global __audio_enabled
+	try:
+		if not __audio_enabled:
+			pygame.mixer.init(22050, -16, 2, 1024) # in pygame 1.9, third argument is stereo if 2.
+			#pygame.mixer.init()
+			__audio_enabled = True
+			print "Audio Enabled."
+	except pygame.error, message :
+		__audio_enabled = False
+		print "Audio disabled:", message
+	return __audio_enabled
 
 def isenabled():
-    return __audio_enabled
+	return __audio_enabled
 
 class NoneSound:
-    def play(self): pass
-    def stop(self): pass
-    def fadeout(self, *args): pass
-    def set_volume(self, *args): pass
-    def get_volume(self, *args): pass
-    def get_num_channels(self, *args): pass
-    def get_length(self, *args): pass
-    def get_buffer(self, *args): pass
+	def play(self): pass
+	def stop(self): pass
+	def fadeout(self, *args): pass
+	def set_volume(self, *args): pass
+	def get_volume(self, *args): pass
+	def get_num_channels(self, *args): pass
+	def get_length(self, *args): pass
+	def get_buffer(self, *args): pass
 
+
+#sound loader
 def load_sound(name, dirname = 'sounds', soundcache = {}):
 
-    if not pygame.mixer or not __audio_enabled:
-        return NoneSound()
+	if not pygame.mixer or not __audio_enabled:
+		return NoneSound()
 	fullname = os.path.join(dirname, name)
 
-    if fullname in soundcache:
-        return soundcache[fullname]
+	if fullname in soundcache:
+		return soundcache[fullname]
 
 	try:
 		sound = pygame.mixer.Sound(fullname)
@@ -245,14 +248,14 @@ class Playlist():
 		self._stop()
 		self.update()
 
-	def add(self, filename, dirname):
-		fullname = os.path.join(dirname, filename)
-        self.playlist.append(fullname)
-        self._resetrandom()
+	def add(self, filename, dirname = "bgm"):
+		fullname = os.path.join( dirname, filename)
+		self.playlist.append(fullname)
+		self._resetrandom()
 
-	def addfolder(self, foldername):
+	def addfolder(self, foldername = "bgm"):
 		for filename in os.listdir(foldername):
-			if filename.endswith(".mp3") or filename.endswith(".wma") or filename.endswith(".m4a"):
+			if filename.endswith(".mp3") or filename.endswith(".wav"):
 				self.add(filename, foldername)
 
 	def setrandom(self):
@@ -278,7 +281,7 @@ class Playlist():
 if __name__ == "__main__":
 	player = Playlist()
 
-	player.addfolder('/home/pi/HousePi/Musicas')
+	player.addfolder()
 
 	def playloop(player):
 		player.play()
@@ -289,5 +292,6 @@ if __name__ == "__main__":
 	import code
 	shell = code.InteractiveConsole(globals())
 	shell.interact()
+
 
 
