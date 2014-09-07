1d0e6cd4662dca7b6e4422a2effaa40230e678e7
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-09-07 15:10:01 -0300

    Modified Email.py

--- Servidor/Email.py
+++ Servidor/Email.py
@@ -83,21 +83,24 @@ class Email(Base.Base):
             
             rows = self.consultarRegistros("select * from Camera") 
         
-            self.camera.desligar()
-            
-            for row in rows:    
-                anexo = "Imagens/" + str(row["Nome"]) + "_" + datetime.datetime.now().strftime("%d-%m-%Y_%H%M%S.jpg")
-                device = str(row["Device"])
-                self.camera.CapturarImagem(device, anexo)
+            try:
+                self.camera.desligar()
                 
-                if anexo != '':
-                    part.set_payload(open(anexo, 'rb').read())
-                    part.add_header('Content-Disposition', 'attachment; filename="%s"' %  os.path.basename(anexo))
+                for row in rows:    
+                    anexo = "Imagens/" + str(row["Nome"]) + "_" + datetime.datetime.now().strftime("%d-%m-%Y_%H%M%S.jpg")
+                    device = str(row["Device"])
+                    self.camera.CapturarImagem(device, anexo)
+                    
+                    if anexo != '':
+                        part.set_payload(open(anexo, 'rb').read())
+                        part.add_header('Content-Disposition', 'attachment; filename="%s"' %  os.path.basename(anexo))
+                    
+                    msg.attach(part)
+                    Encoders.encode_base64(part)
                 
-                msg.attach(part)
-                Encoders.encode_base64(part)
-            
-            self.camera.ligar()
+                self.camera.ligar()
+            except:
+                
             
             mailServer = smtplib.SMTP(msg['SMTP'], int(self.portaSMTP))
             mailServer.ehlo()
@@ -109,7 +112,6 @@ class Email(Base.Base):
             
         except Exception, e:
             print "Erro no envio do e-mail: ", e
-            self.camera.ligar()
         else:
             print "E-mail enviado!"
     
