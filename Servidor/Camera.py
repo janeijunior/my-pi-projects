d90722722d2376f5ee69d89cc74a09b70b784424
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-09-07 15:03:17 -0300

    Modified Camera.py

--- Servidor/Camera.py
+++ Servidor/Camera.py
@@ -51,5 +51,5 @@ class Camera(Base.Base):
     #captura uma imagem do dispositivo (/dev/video0) com o nome e extensão (foto.jpg) passado por parametro
     def CapturarImagem(self, device, nome):
         self.desligar()
-        os.system("sudo fswebcam -r 320x240 -d " + str(device) + " " + str(nome)) 
+        os.system("sudo fswebcam -r 320x240 -d " + str(device) + " --no-banner " + str(nome)) 
         self.ligar()
