1c2dcab3e9918c4071499d8faea325d19a730d4a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-09-07 14:52:03 -0300

    Modified Camera.py

--- Servidor/Camera.py
+++ Servidor/Camera.py
@@ -48,6 +48,8 @@ class Camera(Base.Base):
     def adicionarConexao(self, cliente):
         self.conexoes.insert(len(self.conexoes) + 1, cliente)
     
-    #captura uma imagem com o nome passado por parametro
+    #captura uma imagem com o nome e extensão passado por parametro
     def CapturarImagem(self, nome):
-        
+        self.desligar()
+        os.system("sudo " + str(nome)) 
+        self.ligar()
