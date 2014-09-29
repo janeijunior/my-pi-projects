b6bb09d2fdae8418242abe77a4ce441466cec5bb
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-09-28 22:55:00 -0300

    Modified Servidor.py

--- Servidor/Servidor.py
+++ Servidor/Servidor.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         
     #valida autorizacao de abertura
     def validarAbertura():
-        serial = Funcoes.getSerial() + '1'
+        serial = Funcoes.getSerial()
 
         print serial
         
