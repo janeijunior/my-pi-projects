b5b7cc7d108ed6942493474303f49a8d679cd87a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-10-02 18:56:55 -0300

    Modified Alarme.py

--- Servidor/Alarme.py
+++ Servidor/Alarme.py
@@ -34,7 +34,7 @@ class Alarme(Base.Base):
             self.status = NORMAL
             self.alarmeLigado = True
             
-                
+            
             self.thread = threading.Thread(None, self.__monitorarSensores, None, ())
             self.thread.start()
             
