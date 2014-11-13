c482c1d3eb81e4b1d41eaa017461ab9683ac361a
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-11-12 23:38:11 -0200

    Modified TesteRF.py

--- Servidor/TesteRF.py
+++ Servidor/TesteRF.py
@@ -5,14 +5,14 @@ maxcount = 200
 import RPi.GPIO as GPIO, time, sys
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(21, GPIO.IN)
+GPIO.setup(4, GPIO.IN)
 
 while True:
-  pin = GPIO.input(21)
+  pin = GPIO.input(4)
   if pin:
     count = 0
     while (count < maxcount):
-      pin = GPIO.input(21)
+      pin = GPIO.input(4)
       sys.stdout.write(str(int(pin)))
       pin = 0
       count = count + 1
