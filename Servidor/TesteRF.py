cb9d0aa330442cbb419afda8cd50cbafe31b8e43
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2014-11-12 23:37:23 -0200

    Modified TesteRF.py

--- Servidor/TesteRF.py
+++ Servidor/TesteRF.py
@@ -8,11 +8,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN)
 
 while True:
-  pin = GPIO.input(4)
+  pin = GPIO.input(21)
   if pin:
     count = 0
     while (count < maxcount):
-      pin = GPIO.input(4)
+      pin = GPIO.input(21)
       sys.stdout.write(str(int(pin)))
       pin = 0
       count = count + 1
