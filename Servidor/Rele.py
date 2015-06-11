7f4fc9968534319e57938a0143166d733e6eaa73
Rodrigo Batistello <rodrigobatistello@hotmail.com>
2015-06-10 23:02:13 -0300

    Modified Rele.py

--- Servidor/Rele.py
+++ Servidor/Rele.py
@@ -5,6 +5,7 @@ import Adafruit_MCP230xx
 import Base
 import time
 import thread
+import threading
 
 #variavel para controle dos pinos GPIO (reles)
 mcp = Adafruit_MCP230xx.Adafruit_MCP230XX(address=0x20, num_gpios=16)
